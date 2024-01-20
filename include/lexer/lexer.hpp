67cf6b9fb2092ad2a9853e8a7bc482919d325632
romainducrocq <romain.g.ducrocq@gmail.com>
2024-01-20 21:44:59 +0100

    add boost regex

--- include/lexer/lexer.hpp
+++ include/lexer/lexer.hpp
@@ -5,6 +5,24 @@
 
 namespace Lexer
 {
+    enum TOKEN_KIND {
+        parenthesis_open,
+        parenthesis_close,
+        brace_open,
+        brace_close,
+        semicolon,
+
+        key_int,
+        key_void,
+        key_return,
+
+        identifier,
+        constant,
+
+        skip,
+        error
+    };
+
     void lexing(const std::string& filename);
 }
 
--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,12 +2,41 @@
 #include "util/fopen.hpp"
 
 #include <string>
+#include <array>
+#include <regex>
+
 #include <iostream>
 
 namespace Lexer {
 
+static std::array<std::string, TOKEN_KIND::error+1> TOKEN_REGEX = {
+    R"(\()",
+    R"(\))",
+    R"({)",
+    R"(})",
+    R"(;)",
+    R"(int\b)",
+    R"(void\b)",
+    R"(return\b)",
+    R"([a-zA-Z_]\w*\b)",
+    R"([0-9]+(?![\w.]))",
+    R"([ \n\r\t\f\v])",
+    R"(.)"
+};
+
 static void tokenize(const std::string& filename) {
     Fopen::file_open_read(filename);
+
+    std::string regexp_string = "";
+    for(size_t i = 0; i < TOKEN_KIND::error+1; i++) {
+        // regexp_string += "(?P<" + std::to_string(i) + ">" + TOKEN_REGEX[i] + ")|";
+        regexp_string += "(?P<" + std::to_string(i) + ">" + TOKEN_REGEX[i] + ")|";
+    }
+    regexp_string.pop_back();
+
+    std::cout << regexp_string << std::endl;
+
+    // std::regex regexp_to_match(regexp_string);
 
     std::string line;
     while(Fopen::read_line(line)) {
