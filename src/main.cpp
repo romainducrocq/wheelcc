149449b6139dec8d14cfeeda6a6a0fe99ce2dddc
romainducrocq <romain.g.ducrocq@gmail.com>
2024-01-22 13:49:31 +0100

    add verbose

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,105 @@
 
 #include <iostream>
 
-std::vector<std::string> args;
+static bool VERBOSE = false;
+
+static void verbose(std::string& out, bool end) {
+    if(VERBOSE) {
+        std::cout << out;
+        if(end) {
+            std::cout << std::endl;
+        }
+    }
+}
+
+/** TODO
+cdef void debug_tokens(list[Token] tokens): #
+    if VERBOSE: #
+        pretty_print_tokens(tokens) #
+*/
+
+/** TODO
+cdef void debug_ast(AST ast): #
+    if VERBOSE: #
+        pretty_print_ast(ast) #
+*/
+
+/**  TODO
+cdef void debug_symbol_table(): #
+    if VERBOSE: #
+        pretty_print_symbol_table() #
+*/
+
+/** TODO
+cdef void debug_backend_symbol_table(): #
+    if VERBOSE: #
+        pretty_print_backend_symbol_table() #
+*/
+
+/** TODO
+cdef void debug_asm_code(list[str] asm_code): #
+    verbose("OK") #
+    if VERBOSE: #
+        pretty_print_asm_code(asm_code) #
+*/
+
+/** TODO
+cdef void do_compile(str filename, int32 opt_code, int32 opt_s_code):
+
+    verbose("-- Lexing ... ", end="")
+    cdef list[Token] tokens = lexing(filename)
+    verbose("OK")
+    if opt_code == 255:
+        debug_tokens(tokens) #
+        return
+
+    verbose("-- Parsing ... ", end="")
+    cdef CProgram c_ast = parsing(tokens)
+    verbose("OK")
+    if opt_code == 254:
+        debug_ast(c_ast) #
+        return
+
+    verbose("-- Semantic analysis ... ", end="")
+    analyze_semantic(c_ast)
+    verbose("OK")
+    if opt_code == 253:
+        debug_ast(c_ast) #
+        debug_symbol_table() #
+        return
+
+    verbose("-- TAC representation ... ", end="")
+    cdef TacProgram tac_ast = three_address_code_representation(c_ast)
+    verbose("OK")
+    if opt_code == 252:
+        debug_ast(tac_ast) #
+        debug_symbol_table() #
+        return
+
+    verbose("-- Assembly generation ... ", end="")
+    cdef AsmProgram asm_ast = assembly_generation(tac_ast)
+    verbose("OK")
+    if opt_code == 251:
+        debug_ast(asm_ast) #
+        debug_symbol_table() #
+        debug_backend_symbol_table() #
+        return
+
+    verbose("-- Code emission ... ", end="")
+    if opt_code == 250: #
+        debug_asm_code(code_emission_print(asm_ast)) #
+        return #
+
+    filename = f"{filename.rsplit('.', 1)[0]}.s"
+    code_emission(asm_ast, filename)
+    verbose("OK")
+*/
+
+static void do_compile(const std::string& /*filename*/, int /*opt_code*/, int /*opt_s_code*/) {
+
+}
+
+static std::vector<std::string> args;
 
 static void shift_args(std::string& arg) {
     if(!args.empty()) {
@@ -36,9 +134,7 @@ static void arg_parse(std::string& filename, int& opt_code, int& opt_s_code) {
     opt_s_code = 0;
 }
 
-/**
-TODO
-
+/** TODO
 cdef void entry(list[str] args):
     global VERBOSE
 
@@ -71,12 +167,21 @@ int main(int argc, char **argv) {
     int opt_code;
     int opt_s_code;
     arg_parse(filename, opt_code, opt_s_code);
+    if(opt_code > 0) {
+        VERBOSE = true;
+    }
+
+    do_compile(filename, opt_code, opt_s_code);
+
+    /* TODO */
 
     std::vector<Token> tokens;
     lexing(filename, tokens);
 
+    std::string out;
     for(const auto& token: tokens) {
-        std::cout << token.token_kind << ", " << token.token << std::endl;
+        out = std::to_string(token.token_kind) + ", " + token.token;
+        verbose(out, true);
     }
 
     return 0;
