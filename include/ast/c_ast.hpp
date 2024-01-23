5bef78d2275dbefa935bfff3cb69a384bb80de5a
romainducrocq <romain.g.ducrocq@gmail.com>
2024-01-23 16:24:20 +0100

    [parse] add c ast

--- include/ast/c_ast.hpp
+++ include/ast/c_ast.hpp
@@ -44,6 +44,18 @@ struct CConstLong : CConst {
 };
 
 /** TODO
+cdef class CConstDouble(CConst):
+    cdef public TDouble value
+*/
+struct CConstDouble : CConst {
+    AST_T type() override;
+    CConstDouble() = default;
+    CConstDouble(TDouble value);
+
+    TDouble value;
+};
+
+/** TODO
 cdef class CConstUInt(CConst):
     cdef public TUInt value
 */
@@ -65,18 +77,6 @@ struct CConstULong : CConst {
     CConstULong(TULong value);
 
     TULong value;
-};
-
-/** TODO
-cdef class CConstDouble(CConst):
-    cdef public TDouble value
-*/
-struct CConstDouble : CConst {
-    AST_T type() override;
-    CConstDouble() = default;
-    CConstDouble(TDouble value);
-
-    TDouble value;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -284,23 +284,53 @@ struct CExp : Ast {
 cdef class CConstant(CExp):
     cdef public CConst constant
 */
+struct CConstant : CExp {
+    AST_T type() override;
+    CConstant() = default;
+    CConstant(std::unique_ptr<CConst>&& constant);
+
+    std::unique_ptr<CConst> constant;
+};
 
 /** TODO
 cdef class CVar(CExp):
     cdef public TIdentifier name
 */
+struct CVar : CExp {
+    AST_T type() override;
+    CVar() = default;
+    CVar(TIdentifier&& name);
+
+    TIdentifier name;
+};
 
 /** TODO
 cdef class CCast(CExp):
     cdef public CExp exp
     cdef public Type target_type
 */
+struct CCast : CExp {
+    AST_T type() override;
+    CCast() = default;
+    CCast(std::unique_ptr<CExp>&& exp, std::unique_ptr<Type>&& target_type);
+
+    std::unique_ptr<CExp> exp;
+    std::unique_ptr<Type> target_type;
+};
 
 /** TODO
 cdef class CUnary(CExp):
     cdef public CUnaryOp unary_op
     cdef public CExp exp
 */
+struct CUnary : CExp {
+    AST_T type() override;
+    CUnary() = default;
+    CUnary(std::unique_ptr<CUnaryOp>&& unary_op, std::unique_ptr<CExp>&& exp);
+
+    std::unique_ptr<CUnaryOp> unary_op;
+    std::unique_ptr<CExp> exp;
+};
 
 /** TODO
 cdef class CBinary(CExp):
@@ -308,12 +338,30 @@ cdef class CBinary(CExp):
     cdef public CExp exp_left
     cdef public CExp exp_right
 */
+struct CBinary : CExp {
+    AST_T type() override;
+    CBinary() = default;
+    CBinary(std::unique_ptr<CBinaryOp>&& binary_op, std::unique_ptr<CExp>&& exp_left,
+            std::unique_ptr<CExp>&& exp_right);
+
+    std::unique_ptr<CBinaryOp> binary_op;
+    std::unique_ptr<CExp> exp_left;
+    std::unique_ptr<CExp> exp_right;
+};
 
 /** TODO
 cdef class CAssignment(CExp):
     cdef public CExp exp_left
     cdef public CExp exp_right
 */
+struct CAssignment : CExp {
+    AST_T type() override;
+    CAssignment() = default;
+    CAssignment(std::unique_ptr<CExp>&& exp_left, std::unique_ptr<CExp>&& exp_right);
+
+    std::unique_ptr<CExp> exp_left;
+    std::unique_ptr<CExp> exp_right;
+};
 
 /** TODO
 cdef class CConditional(CExp):
@@ -321,6 +369,16 @@ cdef class CConditional(CExp):
     cdef public CExp exp_middle
     cdef public CExp exp_right
 */
+struct CConditional : CExp {
+    AST_T type() override;
+    CConditional() = default;
+    CConditional(std::unique_ptr<CExp>&& condition, std::unique_ptr<CExp>&& exp_middle,
+                 std::unique_ptr<CExp>&& exp_right);
+
+    std::unique_ptr<CExp> condition;
+    std::unique_ptr<CExp> exp_middle;
+    std::unique_ptr<CExp> exp_right;
+};
 
 /** TODO
 cdef class CAssignmentCompound(CExp):
@@ -328,12 +386,30 @@ cdef class CAssignmentCompound(CExp):
     cdef public CExp exp_left
     cdef public CExp exp_right
 */
+struct CAssignmentCompound : CExp {
+    AST_T type() override;
+    CAssignmentCompound() = default;
+    CAssignmentCompound(std::unique_ptr<CBinaryOp>&& binary_op, std::unique_ptr<CExp>&& exp_left,
+                        std::unique_ptr<CExp>&& exp_right);
+
+    std::unique_ptr<CBinaryOp> binary_op;
+    std::unique_ptr<CExp> exp_left;
+    std::unique_ptr<CExp> exp_right;
+};
 
 /** TODO
 cdef class CFunctionCall(CExp):
     cdef public TIdentifier name
     cdef public list[CExp] args
  */
+struct CFunctionCall : CExp {
+    AST_T type() override;
+    CFunctionCall() = default;
+    CFunctionCall(TIdentifier&& name, std::vector<std::unique_ptr<CExp>>&& args);
+
+    TIdentifier name;
+    std::vector<std::unique_ptr<CExp>> args;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -341,16 +417,33 @@ cdef class CFunctionCall(CExp):
 cdef class CStatement(AST):
     pass
 */
+struct CStatement : Ast {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CReturn(CStatement):
     cdef public CExp exp
 */
+struct CReturn : CStatement {
+    AST_T type() override;
+    CReturn() = default;
+    CReturn(std::unique_ptr<CExp>&& exp);
+
+    std::unique_ptr<CExp> exp;
+};
 
 /** TODO
 cdef class CExpression(CStatement):
     cdef public CExp exp
 */
+struct CExpression : CStatement {
+    AST_T type() override;
+    CExpression() = default;
+    CExpression(std::unique_ptr<CExp>&& exp);
+
+    std::unique_ptr<CExp> exp;
+};
 
 /** TODO
 cdef class CIf(CStatement):
@@ -359,22 +452,56 @@ cdef class CIf(CStatement):
     # Optional
     cdef public CStatement else_fi
 */
+struct CIf : CStatement {
+    AST_T type() override;
+    CIf() = default;
+    CIf(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& then,
+        std::unique_ptr<CStatement>&& else_fi);
+
+    std::unique_ptr<CExp> condition;
+    std::unique_ptr<CStatement> then;
+    // Optional
+    std::unique_ptr<CStatement> else_fi;
+};
 
 /** TODO
 cdef class CGoto(CStatement):
     cdef public TIdentifier target
 */
+struct CGoto : CStatement {
+    AST_T type() override;
+    CGoto() = default;
+    CGoto(TIdentifier&& target);
+
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CLabel(CStatement):
     cdef public TIdentifier target
     cdef public CStatement jump_to
 */
+struct CLabel : CStatement {
+    AST_T type() override;
+    CLabel() = default;
+    CLabel(TIdentifier&& target, std::unique_ptr<CStatement>&& jump_to);
+
+    TIdentifier target;
+    std::unique_ptr<CStatement> jump_to;
+};
 
 /** TODO
 cdef class CCompound(CStatement):
     cdef public CBlock block
 */
+struct CBlock;
+struct CCompound : CStatement {
+    AST_T type() override;
+    CCompound() = default;
+    CCompound(std::unique_ptr<CBlock>&& block);
+
+    std::unique_ptr<CBlock> block;
+};
 
 /** TODO
 cdef class CWhile(CStatement):
@@ -382,6 +509,15 @@ cdef class CWhile(CStatement):
     cdef public CStatement body
     cdef public TIdentifier target
 */
+struct CWhile : CStatement {
+    AST_T type() override;
+    CWhile() = default;
+    CWhile(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& body);
+
+    std::unique_ptr<CExp> condition;
+    std::unique_ptr<CStatement> body;
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CDoWhile(CStatement):
@@ -389,6 +525,15 @@ cdef class CDoWhile(CStatement):
     cdef public CStatement body
     cdef public TIdentifier target
 */
+struct CDoWhile : CStatement {
+    AST_T type() override;
+    CDoWhile() = default;
+    CDoWhile(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& body);
+
+    std::unique_ptr<CExp> condition;
+    std::unique_ptr<CStatement> body;
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CFor(CStatement):
@@ -400,21 +545,49 @@ cdef class CFor(CStatement):
     cdef public CStatement body
     cdef public TIdentifier target
 */
+struct CForInit;
+struct CFor : CStatement {
+    AST_T type() override;
+    CFor() = default;
+    CFor(std::unique_ptr<CForInit>&& init, std::unique_ptr<CExp>&& condition, std::unique_ptr<CExp>&& post,
+         std::unique_ptr<CStatement>&& body);
+
+    std::unique_ptr<CForInit> init;
+    std::unique_ptr<CExp> condition;
+    std::unique_ptr<CExp> post;
+    std::unique_ptr<CStatement> body;
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CBreak(CStatement):
     cdef public TIdentifier target
 */
+struct CBreak : CStatement {
+    AST_T type() override;
+    CBreak() = default;
+
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CContinue(CStatement):
     cdef public TIdentifier target
 */
+struct CContinue : CStatement {
+    AST_T type() override;
+    CContinue() = default;
+
+    TIdentifier target;
+};
 
 /** TODO
 cdef class CNull(CStatement):
     pass
 */
+struct CNull : CStatement {
+    AST_T type() override;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -422,17 +595,36 @@ cdef class CNull(CStatement):
 cdef class CForInit(AST):
     pass
 */
+struct CForInit : Ast {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CInitDecl(CForInit):
     cdef public CVariableDeclaration init
 */
+struct CVariableDeclaration;
+struct CInitDecl : CForInit {
+    AST_T type() override;
+    CInitDecl() = default;
+    CInitDecl(std::unique_ptr<CVariableDeclaration>&& init);
+
+    std::unique_ptr<CVariableDeclaration> init;
+};
 
 /** TODO
 cdef class CInitExp(CForInit):
     # Optional
     cdef public CExp init
 */
+struct CInitExp : CForInit {
+    AST_T type() override;
+    CInitExp() = default;
+    CInitExp(std::unique_ptr<CExp>&& init);
+
+    // Optional
+    std::unique_ptr<CExp> init;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -440,11 +632,22 @@ cdef class CInitExp(CForInit):
 cdef class CBlock(AST):
     pass
 */
+struct CBlock : Ast {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CB(CBlock):
     cdef public list[CBlockItem] block_items
 */
+struct CBlockItem;
+struct CB : CBlock {
+    AST_T type() override;
+    CB() = default;
+    CB(std::vector<std::unique_ptr<CBlockItem>>&& block_items);
+
+    std::vector<std::unique_ptr<CBlockItem>> block_items;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -452,16 +655,34 @@ cdef class CB(CBlock):
 cdef class CBlockItem(AST):
     pass
 */
+struct CBlockItem : Ast {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CS(CBlockItem):
     cdef public CStatement statement
 */
+struct CS : CBlockItem {
+    AST_T type() override;
+    CS() = default;
+    CS(std::unique_ptr<CStatement>&& statement);
+
+    std::unique_ptr<CStatement> statement;
+};
 
 /** TODO
 cdef class CD(CBlockItem):
     cdef public CDeclaration declaration
 */
+struct CDeclaration;
+struct CD : CBlockItem {
+    AST_T type() override;
+    CD() = default;
+    CD(std::unique_ptr<CDeclaration>&& declaration);
+
+    std::unique_ptr<CDeclaration> declaration;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -469,16 +690,25 @@ cdef class CD(CBlockItem):
 cdef class CStorageClass(AST):
     pass
 */
+struct CStorageClass : Ast {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CStatic(CStorageClass):
     pass
 */
+struct CStatic : CStorageClass {
+    AST_T type() override;
+};
 
 /** TODO
 cdef class CExtern(CStorageClass):
     pass
 */
+struct CExtern : CStorageClass {
+    AST_T type() override;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -492,6 +722,21 @@ cdef class CFunctionDeclaration(AST):
     # Optional
     cdef public CStorageClass storage_class
 */
+struct CFunctionDeclaration : Ast {
+    AST_T type() override;
+    CFunctionDeclaration() = default;
+    CFunctionDeclaration(TIdentifier&& target, std::vector<std::unique_ptr<TIdentifier>>&& params,
+                         std::unique_ptr<CBlock>&& body, std::unique_ptr<Type>&& fun_type,
+                         std::unique_ptr<CStorageClass>&& storage_class);
+
+    TIdentifier target;
+    std::vector<std::unique_ptr<TIdentifier>> params;
+    // Optional
+    std::unique_ptr<CBlock> body;
+    std::unique_ptr<Type> fun_type;
+    // Optional
+    std::unique_ptr<CStorageClass> storage_class;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -504,6 +749,17 @@ cdef class CVariableDeclaration(AST):
     # Optional
     cdef public CStorageClass storage_class
 */
+struct CVariableDeclaration : Ast {
+    AST_T type() override;
+    CVariableDeclaration() = default;
+    CVariableDeclaration(TIdentifier&& target, std::unique_ptr<CExp>&& init, std::unique_ptr<Type>&& var_type,
+                         std::unique_ptr<CStorageClass>&& storage_class);
+
+    TIdentifier target;
+    std::unique_ptr<CExp> init;
+    std::unique_ptr<Type> var_type;
+    std::unique_ptr<CStorageClass> storage_class;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -511,17 +767,33 @@ cdef class CVariableDeclaration(AST):
 cdef class CDeclaration(AST):
     pass
 */
+struct CDeclaration : Ast {
+    AST_T type() override;
+};
 
 /** TODO
-
 cdef class CFunDecl(CDeclaration):
     cdef public CFunctionDeclaration function_decl
 */
+struct CFunDecl : CDeclaration {
+    AST_T type() override;
+    CFunDecl() = default;
+    CFunDecl(std::unique_ptr<CFunctionDeclaration>&& function_decl);
+
+    std::unique_ptr<CFunctionDeclaration> function_decl;
+};
 
 /** TODO
 cdef class CVarDecl(CDeclaration):
     cdef public CVariableDeclaration variable_decl
 */
+struct CVarDecl : CDeclaration {
+    AST_T type() override;
+    CVarDecl() = default;
+    CVarDecl(std::unique_ptr<CVariableDeclaration>&& variable_decl);
+
+    std::unique_ptr<CVariableDeclaration> variable_decl;
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -529,5 +801,12 @@ cdef class CVarDecl(CDeclaration):
 cdef class CProgram(AST):
     cdef public list[CDeclaration] declarations
 */
+struct CProgram : Ast {
+    AST_T type() override;
+    CProgram() = default;
+    CProgram(std::vector<std::unique_ptr<CDeclaration>>&& declarations);
+
+    std::vector<std::unique_ptr<CDeclaration>> declarations;
+};
 
 #endif
---
+++ src/ast/c_ast.cpp
@@ -0,0 +1,182 @@
+#include "ast/c_ast.hpp"
+#include "ast/ast.hpp"
+#include "ast/symbol_table.hpp"
+
+#include <memory>
+#include <vector>
+
+AST_T CConst::type() { return AST_T::CConst_t; }
+AST_T CConstInt::type() { return AST_T::CConstInt_t; }
+AST_T CConstLong::type() { return AST_T::CConstLong_t; }
+AST_T CConstDouble::type() { return AST_T::CConstDouble_t; }
+AST_T CConstUInt::type() { return AST_T::CConstUInt_t; }
+AST_T CConstULong::type() { return AST_T::CConstULong_t; }
+AST_T CUnaryOp::type() { return AST_T::CUnaryOp_t; }
+AST_T CComplement::type() { return AST_T::CComplement_t; }
+AST_T CNegate::type() { return AST_T::CNegate_t; }
+AST_T CNot::type() { return AST_T::CNot_t; }
+AST_T CBinaryOp::type() { return AST_T::CBinaryOp_t; }
+AST_T CAdd::type() { return AST_T::CAdd_t; }
+AST_T CSubtract::type() { return AST_T::CSubtract_t; }
+AST_T CMultiply::type() { return AST_T::CMultiply_t; }
+AST_T CDivide::type() { return AST_T::CDivide_t; }
+AST_T CRemainder::type() { return AST_T::CRemainder_t; }
+AST_T CBitAnd::type() { return AST_T::CBitAnd_t; }
+AST_T CBitOr::type() { return AST_T::CBitOr_t; }
+AST_T CBitXor::type() { return AST_T::CBitXor_t; }
+AST_T CBitShiftLeft::type() { return AST_T::CBitShiftLeft_t; }
+AST_T CBitShiftRight::type() { return AST_T::CBitShiftRight_t; }
+AST_T CAnd::type() { return AST_T::CAnd_t; }
+AST_T COr::type() { return AST_T::COr_t; }
+AST_T CEqual::type() { return AST_T::CEqual_t; }
+AST_T CNotEqual::type() { return AST_T::CNotEqual_t; }
+AST_T CLessThan::type() { return AST_T::CLessThan_t; }
+AST_T CLessOrEqual::type() { return AST_T::CLessOrEqual_t; }
+AST_T CGreaterThan::type() { return AST_T::CGreaterThan_t; }
+AST_T CGreaterOrEqual::type() { return AST_T::CGreaterOrEqual_t; }
+AST_T CExp::type() { return AST_T::CExp_t; }
+AST_T CConstant::type() { return AST_T::CConstant_t; }
+AST_T CVar::type() { return AST_T::CVar_t; }
+AST_T CCast::type() { return AST_T::CCast_t; }
+AST_T CUnary::type() { return AST_T::CUnary_t; }
+AST_T CBinary::type() { return AST_T::CBinary_t; }
+AST_T CAssignment::type() { return AST_T::CAssignment_t; }
+AST_T CConditional::type() { return AST_T::CConditional_t; }
+AST_T CAssignmentCompound::type() { return AST_T::CAssignmentCompound_t; }
+AST_T CFunctionCall::type() { return AST_T::CFunctionCall_t; }
+AST_T CStatement::type() { return AST_T::CStatement_t; }
+AST_T CReturn::type() { return AST_T::CReturn_t; }
+AST_T CExpression::type() { return AST_T::CExpression_t; }
+AST_T CIf::type() { return AST_T::CIf_t; }
+AST_T CGoto::type() { return AST_T::CGoto_t; }
+AST_T CLabel::type() { return AST_T::CLabel_t; }
+AST_T CCompound::type() { return AST_T::CCompound_t; }
+AST_T CWhile::type() { return AST_T::CWhile_t; }
+AST_T CDoWhile::type() { return AST_T::CDoWhile_t; }
+AST_T CFor::type() { return AST_T::CFor_t; }
+AST_T CBreak::type() { return AST_T::CBreak_t; }
+AST_T CContinue::type() { return AST_T::CContinue_t; }
+AST_T CNull::type() { return AST_T::CNull_t; }
+AST_T CForInit::type() { return AST_T::CForInit_t; }
+AST_T CInitDecl::type() { return AST_T::CInitDecl_t; }
+AST_T CInitExp::type() { return AST_T::CInitExp_t; }
+AST_T CBlock::type() { return AST_T::CBlock_t; }
+AST_T CB::type() { return AST_T::CB_t; }
+AST_T CBlockItem::type() { return AST_T::CBlockItem_t; }
+AST_T CS::type() { return AST_T::CS_t; }
+AST_T CD::type() { return AST_T::CD_t; }
+AST_T CStorageClass::type() { return AST_T::CStorageClass_t; }
+AST_T CStatic::type() { return AST_T::CStatic_t; }
+AST_T CExtern::type() { return AST_T::CExtern_t; }
+AST_T CFunctionDeclaration::type() { return AST_T::CFunctionDeclaration_t; }
+AST_T CVariableDeclaration::type() { return AST_T::CVariableDeclaration_t; }
+AST_T CDeclaration::type() { return AST_T::CDeclaration_t; }
+AST_T CFunDecl::type() { return AST_T::CFunDecl_t; }
+AST_T CVarDecl::type() { return AST_T::CVarDecl_t; }
+AST_T CProgram::type() { return AST_T::CProgram_t; }
+
+CConstInt::CConstInt(TInt value)
+    : value(value) {}
+
+CConstLong::CConstLong(TLong value)
+    : value(value) {}
+
+CConstDouble::CConstDouble(TDouble value)
+    : value(value) {}
+
+CConstUInt::CConstUInt(TUInt value)
+    : value(value) {}
+
+CConstULong::CConstULong(TULong value)
+    : value(value) {}
+
+CConstant::CConstant(std::unique_ptr<CConst>&& constant)
+    : constant(std::move(constant)) {}
+
+CVar::CVar(TIdentifier&& name)
+    : name(std::move(name)) {}
+
+CCast::CCast(std::unique_ptr<CExp>&& exp, std::unique_ptr<Type>&& target_type)
+    : exp(std::move(exp)), target_type(std::move(target_type)) {}
+
+CUnary::CUnary(std::unique_ptr<CUnaryOp>&& unary_op, std::unique_ptr<CExp>&& exp)
+    : unary_op(std::move(unary_op)), exp(std::move(exp)) {}
+
+CBinary::CBinary(std::unique_ptr<CBinaryOp>&& binary_op, std::unique_ptr<CExp>&& exp_left,
+                 std::unique_ptr<CExp>&& exp_right)
+    : binary_op(std::move(binary_op)), exp_left(std::move(exp_left)), exp_right(std::move(exp_right)) {}
+
+CAssignment::CAssignment(std::unique_ptr<CExp>&& exp_left, std::unique_ptr<CExp>&& exp_right)
+    : exp_left(std::move(exp_left)), exp_right(std::move(exp_right)) {}
+
+CConditional::CConditional(std::unique_ptr<CExp>&& condition, std::unique_ptr<CExp>&& exp_middle,
+                           std::unique_ptr<CExp>&& exp_right)
+    : condition(std::move(condition)), exp_middle(std::move(exp_middle)), exp_right(std::move(exp_right)) {}
+
+CAssignmentCompound::CAssignmentCompound(std::unique_ptr<CBinaryOp>&& binary_op, std::unique_ptr<CExp>&& exp_left,
+                                         std::unique_ptr<CExp>&& exp_right)
+    : binary_op(std::move(binary_op)), exp_left(std::move(exp_left)), exp_right(std::move(exp_right)) {}
+
+CReturn::CReturn(std::unique_ptr<CExp>&& exp)
+    : exp(std::move(exp)) {}
+
+CExpression::CExpression(std::unique_ptr<CExp>&& exp)
+    : exp(std::move(exp)) {}
+
+CIf::CIf(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& then, std::unique_ptr<CStatement>&& else_fi)
+    : condition(std::move(condition)), then(std::move(then)), else_fi(std::move(else_fi)) {}
+
+CGoto::CGoto(TIdentifier&& target)
+    : target(std::move(target)) {}
+
+CLabel::CLabel(TIdentifier&& target, std::unique_ptr<CStatement>&& jump_to)
+    : target(std::move(target)), jump_to(std::move(jump_to)) {}
+
+CCompound::CCompound(std::unique_ptr<CBlock>&& block)
+    : block(std::move(block)) {}
+
+CWhile::CWhile(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& body)
+    : condition(std::move(condition)), body(std::move(body)) {}
+
+CDoWhile::CDoWhile(std::unique_ptr<CExp>&& condition, std::unique_ptr<CStatement>&& body)
+    : condition(std::move(condition)), body(std::move(body)) {}
+
+CFor::CFor(std::unique_ptr<CForInit>&& init, std::unique_ptr<CExp>&& condition, std::unique_ptr<CExp>&& post,
+           std::unique_ptr<CStatement>&& body)
+    : init(std::move(init)), condition(std::move(condition)), post(std::move(post)), body(std::move(body)) {}
+
+CInitDecl::CInitDecl(std::unique_ptr<CVariableDeclaration>&& init)
+    : init(std::move(init)) {}
+
+CInitExp::CInitExp(std::unique_ptr<CExp>&& init)
+    : init(std::move(init)) {}
+
+CB::CB(std::vector<std::unique_ptr<CBlockItem>>&& block_items)
+    : block_items(std::move(block_items)) {}
+
+CS::CS(std::unique_ptr<CStatement>&& statement)
+    : statement(std::move(statement)) {}
+
+CD::CD(std::unique_ptr<CDeclaration>&& declaration)
+    : declaration(std::move(declaration)) {}
+
+CFunctionDeclaration::CFunctionDeclaration(TIdentifier&& target, std::vector<std::unique_ptr<TIdentifier>>&& params,
+                                           std::unique_ptr<CBlock>&& body, std::unique_ptr<Type>&& fun_type,
+                                           std::unique_ptr<CStorageClass>&& storage_class)
+    : target(std::move(target)), params(std::move(params)), body(std::move(body)), fun_type(std::move(fun_type)),
+      storage_class(std::move(storage_class)) {}
+
+CVariableDeclaration::CVariableDeclaration(TIdentifier&& target, std::unique_ptr<CExp>&& init,
+                                           std::unique_ptr<Type>&& var_type,
+                                           std::unique_ptr<CStorageClass>&& storage_class)
+    :  target(std::move(target)), init(std::move(init)), var_type(std::move(var_type)),
+       storage_class(std::move(storage_class)) {}
+
+CFunDecl::CFunDecl(std::unique_ptr<CFunctionDeclaration>&& function_decl)
+    : function_decl(std::move(function_decl)) {}
+
+CVarDecl::CVarDecl(std::unique_ptr<CVariableDeclaration>&& variable_decl)
+    : variable_decl(std::move(variable_decl)) {}
+
+CProgram::CProgram(std::vector<std::unique_ptr<CDeclaration>>&& declarations)
+    : declarations(std::move(declarations)) {}
