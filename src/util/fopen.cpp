115c34be2137089513ca37ae26ee00f1e6b5b56a
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-13 16:06:13 +0100

    [emit] fwrite init

--- src/util/fopen.cpp
+++ src/util/fopen.cpp
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <string>
 
-static FILE* file_in = nullptr;
 static size_t line_number = 0;
+static FILE* file_in = nullptr;
+static FILE* file_out = nullptr;
 
 void file_open_read(const std::string& filename) {
     file_in = nullptr;
@@ -16,6 +17,23 @@ void file_open_read(const std::string& filename) {
     }
     set_filename(filename);
 }
+
+/** TODO
+cdef void file_open_write(str filename):
+    global c_file_out
+    global stream_buf
+    c_file_out = NULL
+    stream_buf = ""
+
+    cdef bytes b_filename = filename.encode("UTF-8")
+    cdef char *c_filename = b_filename
+
+    c_file_out = fopen(c_filename, "wb")
+    if c_file_out == NULL:
+
+        raise RuntimeError(
+            f"File {filename} does not exist")
+*/
 
 bool read_line(std::string& line) {
     size_t l = 0;
@@ -35,6 +53,37 @@ size_t get_line_number() {
     return line_number;
 }
 
+/** TODO
+cdef void write_chunk(bytes chunk_fp, size_t chunk_l):
+
+    cdef char *c_chunk_fp = chunk_fp
+    fwrite(c_chunk_fp, sizeof(char), chunk_l, c_file_out)
+*/
+
+/** TODO
+cdef void write_file(str stream, Py_ssize_t chunk_size = 4096):
+    global stream_buf
+
+    stream_buf += stream
+    while len(stream_buf) >= chunk_size:
+        write_chunk(stream_buf[:chunk_size].encode("UTF-8"),
+                    chunk_size)
+
+        stream_buf = stream_buf[chunk_size:]
+*/
+
+/** TODO
+cdef void write_line(str line):
+    write_file(line + "\n")
+*/
+
 void file_close_read() {
     fclose(file_in);
 }
+
+/** TODO
+cdef void file_close_write():
+
+    write_chunk(stream_buf.encode("UTF-8"), len(stream_buf))
+    fclose(c_file_out)
+*/
