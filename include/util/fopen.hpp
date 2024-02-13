6ac5f4c7cc528f4a92175220a93f32fb17099f13
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-13 16:21:14 +0100

    [emit] fwrite

--- include/util/fopen.hpp
+++ include/util/fopen.hpp
@@ -3,9 +3,12 @@
 
 #include <string>
 
-void file_open_read(const std::string& filename);
-bool read_line(std::string& line);
 size_t get_line_number();
+void file_open_read(const std::string& filename);
+void file_open_write(const std::string& filename);
+bool read_line(std::string& line);
+void write_line(std::string&& line);
 void file_close_read();
+void file_close_write();
 
 #endif
--- src/util/fopen.cpp
+++ src/util/fopen.cpp
@@ -5,8 +5,14 @@
 #include <string>
 
 static size_t line_number = 0;
+static std::string stream_buf = "";
+
 static FILE* file_in = nullptr;
 static FILE* file_out = nullptr;
+
+size_t get_line_number() {
+    return line_number;
+}
 
 void file_open_read(const std::string& filename) {
     file_in = nullptr;
@@ -18,7 +24,7 @@ void file_open_read(const std::string& filename) {
     set_filename(filename);
 }
 
-/** TODO
+/**
 cdef void file_open_write(str filename):
     global c_file_out
     global stream_buf
@@ -34,6 +40,15 @@ cdef void file_open_write(str filename):
         raise RuntimeError(
             f"File {filename} does not exist")
 */
+void file_open_write(const std::string& filename) {
+    file_out = nullptr;
+
+    file_out = fopen(filename.c_str(), "wb");
+    if(file_out == nullptr) {
+        raise_runtime_error("File " + em(filename) + " was not created");
+    }
+    stream_buf = "";
+}
 
 bool read_line(std::string& line) {
     size_t l = 0;
@@ -49,16 +64,15 @@ bool read_line(std::string& line) {
     return true;
 }
 
-size_t get_line_number() {
-    return line_number;
-}
-
-/** TODO
+/**
 cdef void write_chunk(bytes chunk_fp, size_t chunk_l):
 
     cdef char *c_chunk_fp = chunk_fp
     fwrite(c_chunk_fp, sizeof(char), chunk_l, c_file_out)
 */
+static void write_chunk(const std::string& chunk_fp, size_t chunk_l) {
+    fwrite(chunk_fp.c_str(), sizeof(char), chunk_l, file_out);
+}
 
 /** TODO
 cdef void write_file(str stream, Py_ssize_t chunk_size = 4096):
@@ -71,19 +85,34 @@ cdef void write_file(str stream, Py_ssize_t chunk_size = 4096):
 
         stream_buf = stream_buf[chunk_size:]
 */
+static void write_file(std::string&& stream, size_t /*chunk_size*/) {
+    // stream_buf += stream
+    stream_buf += stream;
+    //    while len(stream_buf) >= chunk_size:
+    //        write_chunk(stream_buf[:chunk_size].encode("UTF-8"),
+    //                    chunk_size)
+    //        stream_buf = stream_buf[chunk_size:]
+}
 
-/** TODO
+/**
 cdef void write_line(str line):
     write_file(line + "\n")
 */
+void write_line(std::string&& line) {
+    write_file(line + "\n", 4096);
+}
 
 void file_close_read() {
     fclose(file_in);
 }
 
-/** TODO
+/**
 cdef void file_close_write():
 
     write_chunk(stream_buf.encode("UTF-8"), len(stream_buf))
     fclose(c_file_out)
 */
+void file_close_write() {
+    write_chunk(stream_buf, stream_buf.size());
+    fclose(file_out);
+}
