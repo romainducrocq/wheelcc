e38dac00d04a4c7609105785994c1c74ccd9ad5c
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-15 09:48:24 +0100

    clean

--- src/assembly/registers.cpp
+++ src/assembly/registers.cpp
@@ -5,58 +5,6 @@
 
 #include <memory>
 
-/**
-cdef AsmRegister generate_register(int32 register_kind):
-    # reg = AX | CX | DX | DI | SI | R8 | R9 | R10 | R11 | SP | XMM0 | XMM1 | XMM2 | XMM3 | XMM4 | XMM5 | XMM6 | XMM7
-    #     | XMM14 | XMM15
-    cdef AsmReg reg
-    if register_kind == REGISTER_KIND.get('Ax'):
-        reg = AsmAx()
-    elif register_kind == REGISTER_KIND.get('Cx'):
-        reg = AsmCx()
-    elif register_kind == REGISTER_KIND.get('Dx'):
-        reg = AsmDx()
-    elif register_kind == REGISTER_KIND.get('Di'):
-        reg = AsmDi()
-    elif register_kind == REGISTER_KIND.get('Si'):
-        reg = AsmSi()
-    elif register_kind == REGISTER_KIND.get('R8'):
-        reg = AsmR8()
-    elif register_kind == REGISTER_KIND.get('R9'):
-        reg = AsmR9()
-    elif register_kind == REGISTER_KIND.get('R10'):
-        reg = AsmR10()
-    elif register_kind == REGISTER_KIND.get('R11'):
-        reg = AsmR11()
-    elif register_kind == REGISTER_KIND.get('Sp'):
-        reg = AsmSp()
-    elif register_kind == REGISTER_KIND.get('Xmm0'):
-        reg = AsmXMM0()
-    elif register_kind == REGISTER_KIND.get('Xmm1'):
-        reg = AsmXMM1()
-    elif register_kind == REGISTER_KIND.get('Xmm2'):
-        reg = AsmXMM2()
-    elif register_kind == REGISTER_KIND.get('Xmm3'):
-        reg = AsmXMM3()
-    elif register_kind == REGISTER_KIND.get('Xmm4'):
-        reg = AsmXMM4()
-    elif register_kind == REGISTER_KIND.get('Xmm5'):
-        reg = AsmXMM5()
-    elif register_kind == REGISTER_KIND.get('Xmm6'):
-        reg = AsmXMM6()
-    elif register_kind == REGISTER_KIND.get('Xmm7'):
-        reg = AsmXMM7()
-    elif register_kind == REGISTER_KIND.get('Xmm14'):
-        reg = AsmXMM14()
-    elif register_kind == REGISTER_KIND.get('Xmm15'):
-        reg = AsmXMM15()
-    else:
-
-        raise RuntimeError(
-            f"An error occurred in register management, unmanaged register [{register_kind}]")
-
-    return AsmRegister(reg)
-*/
 // reg = AX | CX | DX | DI | SI | R8 | R9 | R10 | R11 | SP | XMM0 | XMM1 | XMM2 | XMM3 | XMM4 | XMM5 | XMM6 | XMM7
 //     | XMM14 | XMM15
 std::shared_ptr<AsmRegister> generate_register(REGISTER_KIND register_kind) {
--- src/assembly/st_conv.cpp
+++ src/assembly/st_conv.cpp
@@ -7,19 +7,6 @@
 
 #include <memory>
 
-/**
-cdef AssemblyType convert_backend_assembly_type(str name_str):
-    if isinstance(symbol_table[name_str].type_t, (Int, UInt)):
-        return LongWord()
-    elif isinstance(symbol_table[name_str].type_t, Double):
-        return BackendDouble()
-    elif isinstance(symbol_table[name_str].type_t, (Long, ULong)):
-        return QuadWord()
-    else:
-
-        raise RuntimeError(
-            "An error occurred in backend symbol table conversion, not all nodes were visited")
-*/
 std::shared_ptr<AssemblyType> convert_backend_assembly_type(const TIdentifier& name) {
     switch(symbol_table[name]->type_t->type()) {
         case AST_T::Int_t:
@@ -35,46 +22,17 @@ std::shared_ptr<AssemblyType> convert_backend_assembly_type(const TIdentifier& n
     }
 }
 
-/**
-cdef str symbol = ""
-*/
 static const TIdentifier* p_symbol;
 
-/**
-cdef void add_backend_symbol(BackendSymbol node):
-    backend_symbol_table[symbol] = node
-*/
 static void add_backend_symbol(std::unique_ptr<BackendSymbol>&& node) {
     backend_symbol_table[*p_symbol] = std::move(node);
 }
 
-/**
-cdef void convert_double_static_constant():
-    cdef AssemblyType assembly_type = BackendDouble()
-    cdef bint is_static = True
-    cdef bint is_constant = True
-    add_backend_symbol(BackendObj(assembly_type, is_static, is_constant))
-*/
 static void convert_double_static_constant() {
-    bool is_static = true;
-    bool is_constant = true;
     std::shared_ptr<AssemblyType> assembly_type = std::make_shared<BackendDouble>();
-    add_backend_symbol(std::make_unique<BackendObj>(std::move(is_static), std::move(is_constant),
-                                                          std::move(assembly_type)));
+    add_backend_symbol(std::make_unique<BackendObj>(true, true, std::move(assembly_type)));
 }
 
-/**
-cdef void convert_static_constant_top_level(AsmStaticConstant node):
-    global symbol
-
-    symbol = node.name.str_t
-    if isinstance(node.initial_value, DoubleInit):
-        convert_double_static_constant()
-    else:
-
-        raise RuntimeError(
-            "An error occurred in backend symbol table conversion, not all nodes were visited")
-*/
 static void convert_static_constant_top_level(AsmStaticConstant* node) {
     p_symbol = &node->name;
     switch(node->initial_value->type()) {
@@ -86,15 +44,6 @@ static void convert_static_constant_top_level(AsmStaticConstant* node) {
     }
 }
 
-/**
-cdef void convert_top_level(AsmTopLevel node):
-    if isinstance(node, AsmStaticConstant):
-        convert_static_constant_top_level(node)
-    else:
-
-        raise RuntimeError(
-            "An error occurred in stack management, not all nodes were visited")
-*/
 static void convert_top_level(AsmTopLevel* node) {
     switch(node->type()) {
         case AST_T::AsmStaticConstant_t:
@@ -105,45 +54,18 @@ static void convert_top_level(AsmTopLevel* node) {
     }
 }
 
-/**
-cdef void convert_fun_type(FunAttr node):
-    cdef bint is_defined = node.is_defined
-    add_backend_symbol(BackendFun(is_defined))
-*/
 static void convert_fun_type(FunAttr* node) {
     bool is_defined = node->is_defined;
     add_backend_symbol(std::make_unique<BackendFun>(std::move(is_defined)));
 }
 
-/**
-cdef void convert_obj_type(IdentifierAttr node):
-    cdef AssemblyType assembly_type = convert_backend_assembly_type(symbol)
-    cdef bint is_static = isinstance(node, StaticAttr)
-    cdef bint is_constant = False
-    add_backend_symbol(BackendObj(assembly_type, is_static, is_constant))
-*/
 static void convert_obj_type(IdentifierAttr* node) {
     std::shared_ptr<AssemblyType> assembly_type =  convert_backend_assembly_type(*p_symbol);
     bool is_static = node->type() == AST_T::StaticAttr_t;
-    bool is_constant = false;
-    add_backend_symbol(std::make_unique<BackendObj>(std::move(is_static), std::move(is_constant),
+    add_backend_symbol(std::make_unique<BackendObj>(std::move(is_static), false,
                                                           std::move(assembly_type)));
 }
 
-/**
-cdef void convert_backend_symbol_table(AsmProgram node):
-    global symbol
-
-    cdef Py_ssize_t top_level
-    for top_level in range(len(node.static_constant_top_levels)):
-        convert_top_level(node.static_constant_top_levels[top_level])
-
-    for symbol in symbol_table:
-        if isinstance(symbol_table[symbol].type_t, FunType):
-            convert_fun_type(symbol_table[symbol].attrs)
-        else:
-            convert_obj_type(symbol_table[symbol].attrs)
-*/
 static void convert_program(AsmProgram* node) {
     for(size_t top_level = 0; top_level < node->static_constant_top_levels.size(); top_level++) {
         convert_top_level(node->static_constant_top_levels[top_level].get());
@@ -161,10 +83,6 @@ static void convert_program(AsmProgram* node) {
     p_symbol = nullptr;
 }
 
-/**
-cdef void convert_symbol_table(AsmProgram asm_ast):
-    convert_backend_symbol_table(asm_ast)
-*/
 void convert_symbol_table(AsmProgram* node) {
     convert_program(node);
 }
