7e2a8c195e6c43dff710f1a78d2bfc666ac72c5d
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-22 14:01:49 +0100

    clean

--- src/assembly/registers.cpp
+++ src/assembly/registers.cpp
@@ -10,66 +10,86 @@
 std::shared_ptr<AsmRegister> generate_register(REGISTER_KIND register_kind) {
     std::unique_ptr<AsmReg> reg;
     switch(register_kind) {
-        case REGISTER_KIND::Ax:
+        case REGISTER_KIND::Ax: {
             reg = std::make_unique<AsmAx>();
             break;
-        case REGISTER_KIND::Cx:
+        }
+        case REGISTER_KIND::Cx: {
             reg = std::make_unique<AsmCx>();
             break;
-        case REGISTER_KIND::Dx:
+        }
+        case REGISTER_KIND::Dx: {
             reg = std::make_unique<AsmDx>();
             break;
-        case REGISTER_KIND::Di:
+        }
+        case REGISTER_KIND::Di: {
             reg = std::make_unique<AsmDi>();
             break;
-        case REGISTER_KIND::Si:
+        }
+        case REGISTER_KIND::Si: {
             reg = std::make_unique<AsmSi>();
             break;
-        case REGISTER_KIND::R8:
+        }
+        case REGISTER_KIND::R8: {
             reg = std::make_unique<AsmR8>();
             break;
-        case REGISTER_KIND::R9:
+        }
+        case REGISTER_KIND::R9: {
             reg = std::make_unique<AsmR9>();
             break;
-        case REGISTER_KIND::R10:
+        }
+        case REGISTER_KIND::R10: {
             reg = std::make_unique<AsmR10>();
             break;
-        case REGISTER_KIND::R11:
+        }
+        case REGISTER_KIND::R11: {
             reg = std::make_unique<AsmR11>();
             break;
-        case REGISTER_KIND::Sp:
+        }
+        case REGISTER_KIND::Sp: {
             reg = std::make_unique<AsmSp>();
             break;
-        case REGISTER_KIND::Xmm0:
+        }
+        case REGISTER_KIND::Xmm0: {
             reg = std::make_unique<AsmXMM0>();
             break;
-        case REGISTER_KIND::Xmm1:
+        }
+        case REGISTER_KIND::Xmm1: {
             reg = std::make_unique<AsmXMM1>();
             break;
-        case REGISTER_KIND::Xmm2:
+        }
+        case REGISTER_KIND::Xmm2: {
             reg = std::make_unique<AsmXMM2>();
             break;
-        case REGISTER_KIND::Xmm3:
+        }
+        case REGISTER_KIND::Xmm3: {
             reg = std::make_unique<AsmXMM3>();
             break;
-        case REGISTER_KIND::Xmm4:
+        }
+        case REGISTER_KIND::Xmm4: {
             reg = std::make_unique<AsmXMM4>();
             break;
-        case REGISTER_KIND::Xmm5:
+        }
+        case REGISTER_KIND::Xmm5: {
             reg = std::make_unique<AsmXMM5>();
             break;
-        case REGISTER_KIND::Xmm6:
+        }
+        case REGISTER_KIND::Xmm6: {
             reg = std::make_unique<AsmXMM6>();
             break;
-        case REGISTER_KIND::Xmm7:
+        }
+        case REGISTER_KIND::Xmm7: {
             reg = std::make_unique<AsmXMM7>();
             break;
-        case REGISTER_KIND::Xmm14:
+        }
+        case REGISTER_KIND::Xmm14: {
             reg = std::make_unique<AsmXMM14>();
             break;
-        case REGISTER_KIND::Xmm15:
+        }
+        case REGISTER_KIND::Xmm15: {
             reg = std::make_unique<AsmXMM15>();
             break;
+        }
         default:
             RAISE_INTERNAL_ERROR;
     }
