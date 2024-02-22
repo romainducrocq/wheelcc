4e3aa2fe24b213f274bbf8e231c7aba26ee9d7f5
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-22 13:55:48 +0100

    [14] tac add repr inner names deref, addr

--- src/util/names.cpp
+++ src/util/names.cpp
@@ -30,30 +30,46 @@ TIdentifier represent_label_identifier(const TIdentifier& label) {
 TIdentifier represent_variable_identifier(CExp* node) {
     TIdentifier name;
     switch(node->type()) {
-        case AST_T::CFunctionCall_t:
-            name = "funcall";
-            break;
-        case AST_T::CVar_t:
-            name = "var";
-            break;
-        case AST_T::CConstant_t:
+        case AST_T::CConstant_t: {
             name = "constant";
             break;
-        case AST_T::CCast_t:
+        }
+        case AST_T::CVar_t: {
+            name = "var";
+            break;
+        }
+        case AST_T::CDereference_t: {
+            name = "deref";
+            break;
+        }
+        case AST_T::CAddrOf_t: {
+            name = "addr";
+            break;
+        }
+        case AST_T::CFunctionCall_t: {
+            name = "funcall";
+            break;
+        }
+        case AST_T::CCast_t: {
             name = "cast";
             break;
-        case AST_T::CAssignment_t:
+        }
+        case AST_T::CAssignment_t: {
             name = "assignment";
             break;
-        case AST_T::CUnary_t:
-            name = "unary";
-            break;
-        case AST_T::CBinary_t:
-            name = "binary";
-            break;
-        case AST_T::CConditional_t:
+        }
+        case AST_T::CConditional_t: {
             name = "ternary";
             break;
+        }
+        case AST_T::CUnary_t: {
+            name = "unary";
+            break;
+        }
+        case AST_T::CBinary_t: {
+            name = "binary";
+            break;
+        }
         default:
             RAISE_INTERNAL_ERROR;
     }
