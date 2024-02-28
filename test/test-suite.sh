ceeb4869a63805a825e5d64dfc1f827b0c97f34d
romainducrocq <romain.g.ducrocq@gmail.com>
2024-02-28 11:44:12 +0100

    clean

--- test/test-suite.sh
+++ test/test-suite.sh
@@ -15,9 +15,8 @@ cd ${ROOT}/../writing-a-c-compiler-tests/
 if [ ${#} -ne 0 ]; then
     test ${@}
 else
-    for i in $(seq 1 15); do
-        if [ ${i} -eq 5 ]; then continue; fi
-        test --chapter ${i} --stage parse --latest-only --bitwise --compound --goto --nan
+    for i in $(seq 1 14); do
+        test --chapter ${i} --stage validate --latest-only --bitwise --compound --goto --nan
     done
 fi
 
