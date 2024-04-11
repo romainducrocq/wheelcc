2076426a08bf4ecaf3ae1d6289c5a882972a8472
romainducrocq <romain.g.ducrocq@gmail.com>
2024-04-11 14:20:04 +0200

    clean

--- src/frontend/parser/parser.cpp
+++ src/frontend/parser/parser.cpp
@@ -5,7 +5,6 @@
 #include "ast/front_symt.hpp"
 #include "ast/front_ast.hpp"
 #include "frontend/parser/lexer.hpp"
-#include "frontend/parser/preced.hpp"
 
 #include <inttypes.h>
 #include <string>
@@ -763,6 +762,55 @@ static std::unique_ptr<CConditional> parse_ternary_exp(std::unique_ptr<CExp> exp
     expect_next_is(pop_next(), TOKEN_KIND::ternary_else);
     std::unique_ptr<CExp> exp_right = parse_exp(precedence);
     return std::make_unique<CConditional>(std::move(exp_left), std::move(exp_middle), std::move(exp_right));
+}
+
+static int32_t parse_token_precedence(TOKEN_KIND token_kind) {
+    switch(token_kind) {
+        case TOKEN_KIND::binop_multiplication:
+        case TOKEN_KIND::binop_division:
+        case TOKEN_KIND::binop_remainder:
+            return 50;
+        case TOKEN_KIND::unop_negation:
+        case TOKEN_KIND::binop_addition:
+            return 45;
+        case TOKEN_KIND::binop_bitshiftleft:
+        case TOKEN_KIND::binop_bitshiftright:
+            return 40;
+        case TOKEN_KIND::binop_lessthan:
+        case TOKEN_KIND::binop_lessthanorequal:
+        case TOKEN_KIND::binop_greaterthan:
+        case TOKEN_KIND::binop_greaterthanorequal:
+            return 35;
+        case TOKEN_KIND::binop_equalto:
+        case TOKEN_KIND::binop_notequal:
+            return 30;
+        case TOKEN_KIND::binop_bitand:
+            return 25;
+        case TOKEN_KIND::binop_bitxor:
+            return 20;
+        case TOKEN_KIND::binop_bitor:
+            return 15;
+        case TOKEN_KIND::binop_and:
+            return 10;
+        case TOKEN_KIND::binop_or:
+            return 5;
+        case TOKEN_KIND::ternary_if:
+            return 3;
+        case TOKEN_KIND::assignment_simple:
+        case TOKEN_KIND::assignment_plus:
+        case TOKEN_KIND::assignment_difference:
+        case TOKEN_KIND::assignment_product:
+        case TOKEN_KIND::assignment_quotient:
+        case TOKEN_KIND::assignment_remainder:
+        case TOKEN_KIND::assignment_bitand:
+        case TOKEN_KIND::assignment_bitor:
+        case TOKEN_KIND::assignment_bitxor:
+        case TOKEN_KIND::assignment_bitshiftleft:
+        case TOKEN_KIND::assignment_bitshiftright:
+            return 1;
+        default:
+            return -1;
+    }
 }
 
 // <exp> ::= <cast-exp> | <exp> <binop> <exp> | <exp> "?" <exp> ":" <exp>
