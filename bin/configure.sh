5c904f8a78459c3d572410345cb5468117294290
romainducrocq <romain.g.ducrocq@gmail.com>
2024-06-10 15:19:51 +0200

    add configure

--- bin/configure.sh
+++ bin/configure.sh
@@ -2,27 +2,23 @@
 
 PACKAGE_NAME="wheelcc"
 
-INSTALL_DEPS=0
-dpkg -s gcc | grep Status | grep -q "install ok installed"
-if [ ${?} -ne 0 ]; then
-    INSTALL_DEPS=1
-fi
-dpkg -s g++ | grep Status | grep -q "install ok installed"
-if [ ${?} -ne 0 ]; then
-    INSTALL_DEPS=1
-fi
-dpkg -s cmake | grep Status | grep -q "install ok installed"
-if [ ${?} -ne 0 ]; then
-    INSTALL_DEPS=1
-fi
-if [ ${INSTALL_DEPS} -ne 0 ]; then
-    sudo apt-get update
-    sudo apt-get -y upgrade
-    sudo apt-get -y install \
-        gcc \
-        g++ \
-        cmake
-fi
+function get_install () {
+    PKG="${1}"
+    dpkg -s ${PKG} | grep Status | grep -q "install ok installed"
+    if [ ${?} -ne 0 ]; then
+        if [ ${GET_UPDATE} -eq 0 ]; then
+            sudo apt-get update
+            sudo apt-get -y upgrade
+            GET_UPDATE=1
+        fi
+        sudo apt-get -y install ${PKG}
+    fi
+}
+
+GET_UPDATE=0
+get_install gcc
+get_install g++
+get_install cmake
 
 echo -n "${PACKAGE_NAME}" > ./package_name.txt
 if [ ${?} -ne 0 ]; then exit 1; fi
