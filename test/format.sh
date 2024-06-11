811f5ebedb5b5969cfb1a058bcb444ead2aba47b
romainducrocq <romain.g.ducrocq@gmail.com>
2024-06-11 19:57:50 +0200

    add format

--- test/format.sh
+++ test/format.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 FILE="$(readlink -f ${1})"
+cp ${FILE} .
 
+FILE="$(basename ${FILE})"
 vi ${FILE} +'e ++ff=dos | set ff=unix | wq!'
 clang-format ${FILE} -style=file > format.out.1
+rm ${FILE}
