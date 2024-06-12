e5c3e3fc5f63f89c961d2debdd88057a76a808ea
romainducrocq <romain.g.ducrocq@gmail.com>
2024-06-12 18:19:26 +0200

    add tests

---
+++ test/tests/13_floating-point_numbers/valid/extra_credit/nan__+lm_data.s
@@ -0,0 +1,25 @@
+	.file	"nan__+lm.c"
+	.text
+	.globl	double_isnan
+	.type	double_isnan, @function
+double_isnan:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movsd	%xmm0, -8(%rbp)
+	movsd	-8(%rbp), %xmm0
+	ucomisd	-8(%rbp), %xmm0
+	setp	%al
+	movzbl	%al, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	double_isnan, .-double_isnan
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits
