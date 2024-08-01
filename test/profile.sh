10723902fa9f150865340e76289fc877f3fa31ee
romainducrocq <romain.g.ducrocq@gmail.com>
2024-08-01 19:45:22 +0200

    add option -I

--- test/profile.sh
+++ test/profile.sh
@@ -8,14 +8,10 @@ if [ -z "${1}" ]; then exit 1; fi
 FILE="$(readlink -f ${1%.*})"
 if [ ! -f "${FILE}.c" ]; then exit 1; fi
 
-gcc -E -P ${FILE}.c -o ${FILE}.i
-
 valgrind \
     --tool=callgrind \
     --callgrind-out-file=$(basename ${FILE}).callgrind.out.1 \
-    ${ROOT}/bin/${PACKAGE_NAME} 0 ${FILE}.i > /dev/null 2>&1
-
-rm ${FILE}.i
+    ${ROOT}/bin/${PACKAGE_NAME} 0 ${FILE}.c $(dirname ${FILE})/ > /dev/null 2>&1
 
 exit 0
 
