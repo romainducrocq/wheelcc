458cecd713f6d04a03fcc9bd3d8ea997b7369e2c
romainducrocq <romain.g.ducrocq@gmail.com>
2024-08-05 11:55:19 +0200

    build

--- bin/make.sh
+++ bin/make.sh
@@ -2,6 +2,10 @@
 
 cd ../build/
 ./build.sh
-if [ ${?} -ne 0 ]; then exit 1; fi
+if [ ${?} -ne 0 ]; then
+    echo -e "\033[0;31merror:\033[0m build failed" 1>&2
+    exit 1
+fi
 
+echo -e "build was successful, install with \033[1m‘./install.sh’\033[0m"
 exit 0
--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cmake -G "Unix Makefiles" -S . -B release/ -DCMAKE_BUILD_TYPE=Release -D__NDEBUG__=ON
+if [ ${?} -ne 0 ]; then exit 1; fi
+
 cd release/
 cmake --build . --config Release
+if [ ${?} -ne 0 ]; then exit 1; fi
 
 exit 0
