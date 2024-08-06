d2a92efd617265b0650e4cd94fbd7e8b24073525
romainducrocq <romain.g.ducrocq@gmail.com>
2024-08-06 11:57:15 +0200

    clean error message

--- include/util/throw.hpp
+++ include/util/throw.hpp
@@ -7,10 +7,10 @@
 
 // Throw
 
-std::string em(const std::string& message);
-[[noreturn]] void raise_argument_error(const std::string& message);
-[[noreturn]] void raise_runtime_error(const std::string& message);
-[[noreturn]] void raise_runtime_error_at_line(const std::string& message, size_t line_number);
+std::string em(const std::string& text);
+[[noreturn]] void raise_argument_error(const std::string& error_message);
+[[noreturn]] void raise_runtime_error(const std::string& error_message);
+[[noreturn]] void raise_runtime_error_at_line(const std::string& error_message, size_t line_number);
 [[noreturn]] void raise_internal_error(const char* func, const char* file, int line);
 #define RAISE_INTERNAL_ERROR raise_internal_error(__func__, __FILE__, __LINE__)
 
--- src/util/throw.cpp
+++ src/util/throw.cpp
@@ -38,34 +38,34 @@ static const std::string& get_filename() {
     }
 }
 
-std::string em(const std::string& message) {
-    std::string em_message = "\033[1m‘";
-    em_message += message;
-    em_message += "’\033[0m";
-    return em_message;
+std::string em(const std::string& text) {
+    std::string em_text = "\033[1m‘";
+    em_text += text;
+    em_text += "’\033[0m";
+    return em_text;
 }
 
-[[noreturn]] void raise_argument_error(const std::string& message) {
-    std::string error_message = "\n\033[0;31merror:\033[0m ";
-    error_message += message;
-    error_message += "\n";
-    throw std::runtime_error(std::move(error_message));
+[[noreturn]] void raise_argument_error(const std::string& error_message) {
+    std::string message = "\n\033[0;31merror:\033[0m ";
+    message += error_message;
+    message += "\n";
+    throw std::runtime_error(std::move(message));
 }
 
-[[noreturn]] void raise_runtime_error(const std::string& message) {
+[[noreturn]] void raise_runtime_error(const std::string& error_message) {
     free_resources();
     const std::string& filename = get_filename();
-    std::string error_message = "\n\033[1m";
-    error_message += filename;
-    error_message += ":\033[0m\n\033[0;31merror:\033[0m ";
-    error_message += message;
-    error_message += "\n";
-    throw std::runtime_error(std::move(error_message));
+    std::string message = "\n\033[1m";
+    message += filename;
+    message += ":\033[0m\n\033[0;31merror:\033[0m ";
+    message += error_message;
+    message += "\n";
+    throw std::runtime_error(std::move(message));
 }
 
-[[noreturn]] void raise_runtime_error_at_line(const std::string& message, size_t line_number) {
+[[noreturn]] void raise_runtime_error_at_line(const std::string& error_message, size_t line_number) {
     if (line_number == 0) {
-        raise_runtime_error(message);
+        raise_runtime_error(error_message);
     }
     free_resources();
     const std::string& filename = get_filename();
@@ -75,7 +75,7 @@ std::string em(const std::string& message) {
         char* buffer = nullptr;
         FILE* file_descriptor = fopen(filename.c_str(), "rb");
         if (!file_descriptor) {
-            raise_runtime_error(message);
+            raise_runtime_error(error_message);
         }
         for (size_t i = 0; i < line_number; ++i) {
             if (getline(&buffer, &len, file_descriptor) == -1) {
@@ -83,7 +83,7 @@ std::string em(const std::string& message) {
                 fclose(file_descriptor);
                 buffer = nullptr;
                 file_descriptor = nullptr;
-                raise_runtime_error(message);
+                raise_runtime_error(error_message);
             }
         }
         line = buffer;
@@ -95,27 +95,27 @@ std::string em(const std::string& message) {
             line.pop_back();
         }
     }
-    std::string error_message = "\n\033[1m";
-    error_message += filename;
-    error_message += ":";
-    error_message += std::to_string(line_number);
-    error_message += ":\033[0m\n\033[0;31merror:\033[0m ";
-    error_message += message;
-    error_message += "\nat line ";
-    error_message += std::to_string(line_number);
-    error_message += ": \033[1m";
-    error_message += line;
-    error_message += "\033[0m";
-    throw std::runtime_error(std::move(error_message));
+    std::string message = "\n\033[1m";
+    message += filename;
+    message += ":";
+    message += std::to_string(line_number);
+    message += ":\033[0m\n\033[0;31merror:\033[0m ";
+    message += error_message;
+    message += "\nat line ";
+    message += std::to_string(line_number);
+    message += ": \033[1m";
+    message += line;
+    message += "\033[0m";
+    throw std::runtime_error(std::move(message));
 }
 
 [[noreturn]] void raise_internal_error(const char* func, const char* file, int line) {
     free_resources();
-    std::string error_message = "\n\033[1m";
-    error_message += std::string(file);
-    error_message += ":";
-    error_message += std::to_string(line);
-    error_message += ":\033[0m\n\033[0;31minternal error:\033[0m ";
-    error_message += std::string(func);
-    throw std::runtime_error(std::move(error_message));
+    std::string message = "\n\033[1m";
+    message += std::string(file);
+    message += ":";
+    message += std::to_string(line);
+    message += ":\033[0m\n\033[0;31minternal error:\033[0m ";
+    message += std::string(func);
+    throw std::runtime_error(std::move(message));
 }
