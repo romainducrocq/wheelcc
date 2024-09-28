f63aa104533a8ff7384b95dd63e27b67ba73b6f9
romainducrocq <romain.g.ducrocq@gmail.com>
2024-09-28 17:49:33 +0200

    clean exec before build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+PACKAGE_NAME="$(cat ../bin/package_name.txt)"
+
+if [ -f "../bin/${PACKAGE_NAME}" ]; then
+    rm ../bin/${PACKAGE_NAME}
+fi
+
 cmake -G "Unix Makefiles" -S . -B release/ -DCMAKE_BUILD_TYPE=Release -D__NDEBUG__=ON
 if [ ${?} -ne 0 ]; then exit 1; fi
 
--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+PACKAGE_NAME="$(cat ../bin/package_name.txt)"
+
 cd ../build/
+
+if [ -f "../bin/${PACKAGE_NAME}" ]; then
+    rm ../bin/${PACKAGE_NAME}
+fi
 
 cmake -G "Unix Makefiles" -S . -B test/ -DCMAKE_BUILD_TYPE=Release
 cd test/
