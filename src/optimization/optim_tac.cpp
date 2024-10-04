c5a44ff40c4416fd62a3259054682bb28962506b
romainducrocq <romain.g.ducrocq@gmail.com>
2024-10-04 13:51:48 +0200

    add optimization pipeline

--- src/optimization/optim_tac.cpp
+++ src/optimization/optim_tac.cpp
@@ -1,5 +1,7 @@
 #include <memory>
 
+#include "util/throw.hpp"
+
 #include "ast/interm_ast.hpp"
 
 #include "optimization/optim_tac.hpp"
@@ -22,6 +24,12 @@ OptimTacContext::OptimTacContext(int32_t optim_1_mask) :
 
 // Constant folding
 
+static void fold_constants_list_instructions(const std::vector<std::unique_ptr<TacInstruction>>& list_node) {
+    for (const auto& instruction : list_node) {
+        // TODO
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Copy propagation
@@ -36,10 +44,42 @@ OptimTacContext::OptimTacContext(int32_t optim_1_mask) :
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+static void optimize_function_top_level(TacFunction* node) {
+    context->is_fixed_point = true;
+    do {
+        if (context->enabled_optimizations[0]) {
+            fold_constants_list_instructions(node->body);
+        }
+        if (context->enabled_optimizations[1]) {
+        }
+        if (context->enabled_optimizations[2]) {
+        }
+        if (context->enabled_optimizations[3]) {
+        }
+    }
+    while (!context->is_fixed_point);
+}
+
+static void optimize_top_level(TacTopLevel* node) {
+    switch (node->type()) {
+        case AST_T::TacFunction_t:
+            optimize_function_top_level(static_cast<TacFunction*>(node));
+            break;
+        default:
+            RAISE_INTERNAL_ERROR;
+    }
+}
+
+static void optimize_program(TacProgram* node) {
+    for (const auto& top_level : node->function_top_levels) {
+        optimize_top_level(top_level.get());
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void three_address_code_optimization(TacProgram* node, int32_t optim_1_mask) {
     context = std::make_unique<OptimTacContext>(optim_1_mask);
+    optimize_program(node);
     context.reset();
 }
