8b739e7636a383a8410243759a0545536a46a5c8
romainducrocq <romain.g.ducrocq@gmail.com>
2024-10-04 11:56:16 +0200

    add optimization sources

---
+++ include/optimization/optim_tac.hpp
@@ -0,0 +1,33 @@
+#ifndef _OPTIMIZATION_OPTIM_TAC_HPP
+#define _OPTIMIZATION_OPTIM_TAC_HPP
+
+#include <array>
+#include <inttypes.h>
+
+#include "ast/interm_ast.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Three address code optimization
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Constant folding
+// Copy propagation
+// Unreachable code elimination
+// Dead store elimination
+
+struct OptimTacContext {
+    OptimTacContext(int32_t optim_1_mask);
+
+    bool is_fixed_point;
+    std::array<bool, 4> enabled_optimizations;
+    // Constant folding
+    // Copy propagation
+    // Unreachable code elimination
+    // Dead store elimination
+};
+
+void three_address_code_optimization(TacProgram* node, int32_t optim_1_mask);
+
+#endif
---
+++ include/optimization/reg_alloc.hpp
@@ -0,0 +1,16 @@
+#ifndef _OPTIMIZATION_REG_ALLOC_HPP
+#define _OPTIMIZATION_REG_ALLOC_HPP
+
+#include <inttypes.h>
+
+#include "ast/back_ast.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Register allocation
+
+struct RegAllocContext {};
+
+void register_allocation(AsmProgram* node, int32_t optim_2_code);
+
+#endif
