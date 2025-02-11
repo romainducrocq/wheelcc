b2661eafaf88edf0e7c71a604f2f4e6ff458b601
romainducrocq <romain.g.ducrocq@gmail.com>
2025-02-11 15:14:37 +0100

    add optim arg

--- test/profile.sh
+++ test/profile.sh
@@ -9,14 +9,30 @@ if [ ${?} -ne 0 ]; then
     sudo apt-get install -y valgrind
 fi
 
-if [ -z "${1}" ]; then exit 1; fi
-FILE="$(readlink -f ${1%.*})"
+ARG=${1}
+
+OPTIM="0 0"
+if [ "${1}" = "-O0" ]; then
+    ARG=${2}
+elif [ "${1}" = "-O1" ]; then
+    OPTIM="15 0"
+    ARG=${2}
+elif [ "${1}" = "-O2" ]; then
+    OPTIM="0 2"
+    ARG=${2}
+elif [ "${1}" = "-O3" ]; then
+    OPTIM="15 2"
+    ARG=${2}
+fi
+
+if [ -z "${ARG}" ]; then exit 1; fi
+FILE="$(readlink -f ${ARG%.*})"
 if [ ! -f "${FILE}.c" ]; then exit 1; fi
 
 valgrind \
     --tool=callgrind \
     --callgrind-out-file=$(basename ${FILE}).callgrind.out.1 \
-    ${ROOT}/bin/${PACKAGE_NAME} 0 15 2 ${FILE}.c $(dirname ${FILE})/ > /dev/null 2>&1
+    ${ROOT}/bin/${PACKAGE_NAME} 0 ${OPTIM} ${FILE}.c $(dirname ${FILE})/ > /dev/null 2>&1
 
 exit 0
 
