47d1e89d957532e2aea8d95af7e6b6cdd059f825
romainducrocq <romain.g.ducrocq@gmail.com>
2025-04-01 13:12:31 +0200

    refactor labels

--- include/frontend/intermediate/idents.hpp
+++ include/frontend/intermediate/idents.hpp
@@ -5,39 +5,13 @@
 
 #include "ast/ast.hpp"
 
+#include "intermediate/labels.hpp" // frontend
+
 struct CExp;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Identifiers
-
-enum LABEL_KIND {
-    Land_false,
-    Land_true,
-    Lbreak,
-    Lcase,
-    Lcomisd_nan,
-    Lcontinue,
-    Ldefault,
-    Ldouble,
-    Ldo_while,
-    Ldo_while_start,
-    Lfor,
-    Lfor_start,
-    Lif_else,
-    Lif_false,
-    Lor_false,
-    Lor_true,
-    Lsd2si_after,
-    Lsd2si_out_of_range,
-    Lsi2sd_after,
-    Lsi2sd_out_of_range,
-    Lstring,
-    Lswitch,
-    Lternary_else,
-    Lternary_false,
-    Lwhile
-};
 
 TIdentifier resolve_label_identifier(TIdentifier label);
 TIdentifier resolve_variable_identifier(TIdentifier variable);
---
+++ src/frontend/intermediate/labels.hpp
@@ -0,0 +1,36 @@
+#ifndef _FRONTEND_INTERMEDIATE_LABELS_HPP
+#define _FRONTEND_INTERMEDIATE_LABELS_HPP
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Identifiers
+
+enum LABEL_KIND {
+    Land_false,
+    Land_true,
+    Lbreak,
+    Lcase,
+    Lcomisd_nan,
+    Lcontinue,
+    Ldefault,
+    Ldouble,
+    Ldo_while,
+    Ldo_while_start,
+    Lfor,
+    Lfor_start,
+    Lif_else,
+    Lif_false,
+    Lor_false,
+    Lor_true,
+    Lsd2si_after,
+    Lsd2si_out_of_range,
+    Lsi2sd_after,
+    Lsi2sd_out_of_range,
+    Lstring,
+    Lswitch,
+    Lternary_else,
+    Lternary_false,
+    Lwhile
+};
+
+#endif
