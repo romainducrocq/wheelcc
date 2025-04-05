56ce1221dfb015442b2f3cd2086a1b3f470b22dd
romainducrocq <romain.g.ducrocq@gmail.com>
2025-04-05 21:19:42 +0200

    test 20

--- test/test-suite.sh
+++ test/test-suite.sh
@@ -16,13 +16,18 @@ cd ${TEST_SUITE}
 if [ ${#} -ne 0 ]; then
     test ${@}
 else
-    test --chapter 20 --no-coalescing --int-only --latest-only
-    exit 0
-    for i in $(seq 1 10); do
-        test --chapter ${i} --latest-only #--extra-credit
+    if [ 0 -eq 0 ]; then
+        test --chapter 20 --no-coalescing --int-only --latest-only --extra-credit
+        for i in $(seq 1 10); do
+            test --chapter ${i} --latest-only --extra-credit
+        done
+        exit 0
+    fi
+    for i in $(seq 1 18); do
+        test --chapter ${i} --latest-only --extra-credit
     done
     for i in $(seq 19 19); do
-        : #test --chapter ${i} --extra-credit
+        test --chapter ${i} --extra-credit
     done
 fi
 
