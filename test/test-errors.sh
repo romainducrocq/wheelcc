d6d1b8fe62712e66dc4116568e335d09dbe50f31
romainducrocq <romain.g.ducrocq@gmail.com>
2025-04-29 15:34:47 +0200

    test errors

--- test/test-errors.sh
+++ test/test-errors.sh
@@ -7,18 +7,16 @@ LIGHT_RED='\033[1;31m'
 LIGHT_GREEN='\033[1;32m'
 NC='\033[0m'
 
-TOOLS_DIR="${PWD}/tools/"
+ERRS_IN=$(sed "s|@PACKAGE_PATH@|${PACKAGE_PATH}|g" tools/print_errors.in)
+ERRS_OUT=$(cd tools/; ./print-errors.sh; cd ../)
 
-ERRS_IN=$(sed "s|@PACKAGE_PATH@|${PACKAGE_PATH}|g" ${TOOLS_DIR}/print_errors.in)
-ERRS_OUT=$(cd ${TOOLS_DIR}; ./print-errors.sh; cd ../)
-
-diff -q <(echo "${ERRS_IN}") <(echo "${ERRS_OUT}")
+diff -q <(echo "${ERRS_IN}" | sort) <(echo "${ERRS_OUT}" | sort)
 RETURN=${?}
 if [ ${RETURN} -eq 0 ]; then
     echo -e "${LIGHT_GREEN}PASS: errors in and out are identical${NC}"
 
 else
-    diff <(echo "${ERRS_IN}") <(echo "${ERRS_OUT}")
+    diff <(echo "${ERRS_IN}" | sort) <(echo "${ERRS_OUT}" | sort)
     echo -e "${LIGHT_RED}FAIL: errors in and out differ${NC}"
     exit 1
 fi
