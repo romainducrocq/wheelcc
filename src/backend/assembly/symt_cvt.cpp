797a83ff7fd610381f9900cfe40a3e0e49b32abd
romainducrocq <romain.g.ducrocq@gmail.com>
2025-04-30 17:59:08 +0200

    func ids len -lt 32

--- src/backend/assembly/symt_cvt.cpp
+++ src/backend/assembly/symt_cvt.cpp
@@ -20,7 +20,7 @@ static std::unique_ptr<SymtCvtContext> context;
 
 // Symbol table conversion
 
-static TInt generate_scalar_type_alignment(Type* type) {
+static TInt scalar_type_align(Type* type) {
     switch (type->type()) {
         case AST_T::Char_t:
         case AST_T::SChar_t:
@@ -39,7 +39,7 @@ static TInt generate_scalar_type_alignment(Type* type) {
     }
 }
 
-static TInt generate_array_aggregate_type_alignment(Array* arr_type, TLong& size) {
+static TInt arr_type_align(Array* arr_type, TLong& size) {
     size = arr_type->size;
     while (arr_type->elem_type->type() == AST_T::Array_t) {
         arr_type = static_cast<Array*>(arr_type->elem_type.get());
@@ -62,7 +62,7 @@ static TInt generate_array_aggregate_type_alignment(Array* arr_type, TLong& size
     return alignment;
 }
 
-static TInt generate_structure_aggregate_type_alignment(Structure* struct_type) {
+static TInt struct_type_align(Structure* struct_type) {
     return frontend->struct_typedef_table[struct_type->tag]->alignment;
 }
 
@@ -70,22 +70,22 @@ TInt generate_type_alignment(Type* type) {
     switch (type->type()) {
         case AST_T::Array_t: {
             TLong size;
-            return generate_array_aggregate_type_alignment(static_cast<Array*>(type), size);
+            return arr_type_align(static_cast<Array*>(type), size);
         }
         case AST_T::Structure_t:
-            return generate_structure_aggregate_type_alignment(static_cast<Structure*>(type));
+            return struct_type_align(static_cast<Structure*>(type));
         default:
-            return generate_scalar_type_alignment(type);
+            return scalar_type_align(type);
     }
 }
 
-static std::shared_ptr<ByteArray> convert_array_aggregate_assembly_type(Array* arr_type) {
+static std::shared_ptr<ByteArray> arr_asm_type(Array* arr_type) {
     TLong size;
-    TInt alignment = generate_array_aggregate_type_alignment(arr_type, size);
+    TInt alignment = arr_type_align(arr_type, size);
     return std::make_shared<ByteArray>(std::move(size), std::move(alignment));
 }
 
-static std::shared_ptr<ByteArray> convert_structure_aggregate_assembly_type(Structure* struct_type) {
+static std::shared_ptr<ByteArray> struct_asm_type(Structure* struct_type) {
     TLong size;
     TInt alignment;
     if (frontend->struct_typedef_table.find(struct_type->tag) != frontend->struct_typedef_table.end()) {
@@ -115,38 +115,36 @@ std::shared_ptr<AssemblyType> convert_backend_assembly_type(TIdentifier name) {
         case AST_T::Double_t:
             return std::make_shared<BackendDouble>();
         case AST_T::Array_t:
-            return convert_array_aggregate_assembly_type(
-                static_cast<Array*>(frontend->symbol_table[name]->type_t.get()));
+            return arr_asm_type(static_cast<Array*>(frontend->symbol_table[name]->type_t.get()));
         case AST_T::Structure_t:
-            return convert_structure_aggregate_assembly_type(
-                static_cast<Structure*>(frontend->symbol_table[name]->type_t.get()));
+            return struct_asm_type(static_cast<Structure*>(frontend->symbol_table[name]->type_t.get()));
         default:
             RAISE_INTERNAL_ERROR;
     }
 }
 
-static void convert_backend_symbol(std::unique_ptr<BackendSymbol>&& node) {
+static void cvt_backend_symbol(std::unique_ptr<BackendSymbol>&& node) {
     backend->backend_symbol_table[context->symbol] = std::move(node);
 }
 
-static void convert_double_static_constant() {
+static void dbl_static_const() {
     std::shared_ptr<AssemblyType> assembly_type = std::make_shared<BackendDouble>();
-    convert_backend_symbol(std::make_unique<BackendObj>(true, true, std::move(assembly_type)));
+    cvt_backend_symbol(std::make_unique<BackendObj>(true, true, std::move(assembly_type)));
 }
 
-static void convert_string_static_constant(Array* arr_type) {
-    std::shared_ptr<AssemblyType> assembly_type = convert_array_aggregate_assembly_type(arr_type);
-    convert_backend_symbol(std::make_unique<BackendObj>(true, true, std::move(assembly_type)));
+static void string_static_const(Array* arr_type) {
+    std::shared_ptr<AssemblyType> assembly_type = arr_asm_type(arr_type);
+    cvt_backend_symbol(std::make_unique<BackendObj>(true, true, std::move(assembly_type)));
 }
 
-static void convert_static_constant_top_level(AsmStaticConstant* node) {
+static void static_const_toplvl(AsmStaticConstant* node) {
     context->symbol = node->name;
     switch (node->static_init->type()) {
         case AST_T::DoubleInit_t:
-            convert_double_static_constant();
+            dbl_static_const();
             break;
         case AST_T::StringInit_t:
-            convert_string_static_constant(static_cast<Array*>(frontend->symbol_table[node->name]->type_t.get()));
+            string_static_const(static_cast<Array*>(frontend->symbol_table[node->name]->type_t.get()));
             break;
         default:
             RAISE_INTERNAL_ERROR;
@@ -156,14 +154,14 @@ static void convert_static_constant_top_level(AsmStaticConstant* node) {
 static void convert_top_level(AsmTopLevel* node) {
     switch (node->type()) {
         case AST_T::AsmStaticConstant_t:
-            convert_static_constant_top_level(static_cast<AsmStaticConstant*>(node));
+            static_const_toplvl(static_cast<AsmStaticConstant*>(node));
             break;
         default:
             RAISE_INTERNAL_ERROR;
     }
 }
 
-static void convert_fun_type(FunAttr* node, FunType* fun_type) {
+static void cvt_fun_type(FunAttr* node, FunType* fun_type) {
     if (fun_type->param_reg_mask == NULL_REGISTER_MASK) {
         if (node->is_defined) {
             RAISE_INTERNAL_ERROR;
@@ -176,27 +174,27 @@ static void convert_fun_type(FunAttr* node, FunType* fun_type) {
         fun_type->ret_reg_mask = REGISTER_MASK_FALSE;
     }
     bool is_defined = node->is_defined;
-    convert_backend_symbol(std::make_unique<BackendFun>(std::move(is_defined)));
+    cvt_backend_symbol(std::make_unique<BackendFun>(std::move(is_defined)));
 }
 
-static void convert_obj_type(IdentifierAttr* node) {
+static void cvt_obj_type(IdentifierAttr* node) {
     if (node->type() != AST_T::ConstantAttr_t) {
         std::shared_ptr<AssemblyType> assembly_type = convert_backend_assembly_type(context->symbol);
         bool is_static = node->type() == AST_T::StaticAttr_t;
-        convert_backend_symbol(std::make_unique<BackendObj>(std::move(is_static), false, std::move(assembly_type)));
+        cvt_backend_symbol(std::make_unique<BackendObj>(std::move(is_static), false, std::move(assembly_type)));
     }
 }
 
-static void convert_program(AsmProgram* node) {
+static void cvt_program(AsmProgram* node) {
     backend->backend_symbol_table.reserve(frontend->symbol_table.size());
     for (const auto& symbol : frontend->symbol_table) {
         context->symbol = symbol.first;
         if (symbol.second->type_t->type() == AST_T::FunType_t) {
-            convert_fun_type(
+            cvt_fun_type(
                 static_cast<FunAttr*>(symbol.second->attrs.get()), static_cast<FunType*>(symbol.second->type_t.get()));
         }
         else {
-            convert_obj_type(symbol.second->attrs.get());
+            cvt_obj_type(symbol.second->attrs.get());
         }
     }
 
@@ -209,6 +207,6 @@ static void convert_program(AsmProgram* node) {
 
 void convert_symbol_table(AsmProgram* node) {
     context = std::make_unique<SymtCvtContext>();
-    convert_program(node);
+    cvt_program(node);
     context.reset();
 }
