9b74de0b99d18bbb281ed4a2892feb68e5b015bc
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-08 22:27:32 +0200

    add ABORT_IF debug macro

--- include/util/throw.hpp
+++ include/util/throw.hpp
@@ -34,12 +34,20 @@ size_t handle_error_at_line(size_t total_linenum);
 [[noreturn]] void raise_base_error(const char* error_msg);
 [[noreturn]] void raise_runtime_error(const char* error_msg);
 [[noreturn]] void raise_runtime_error_at_line(const char* error_msg, size_t linenum);
+#define GET_ERROR_MSG(X, ...) snprintf(errors->msg, sizeof(char) * 1024, X, __VA_ARGS__)
 #define RAISE_INTERNAL_ERROR raise_internal_error(__func__, __FILE__, __LINE__)
 #define RAISE_FATAL_ERROR(X) X > 0 ? raise_base_error(errors->msg) : RAISE_INTERNAL_ERROR
 #define RAISE_ARGUMENT_ERROR(X) X > 0 ? raise_base_error(errors->msg) : RAISE_INTERNAL_ERROR
 #define RAISE_RUNTIME_ERROR(X) X > 0 ? raise_runtime_error(errors->msg) : RAISE_INTERNAL_ERROR
 #define RAISE_RUNTIME_ERROR_AT(X, Y) X > 0 ? raise_runtime_error_at_line(errors->msg, Y) : RAISE_INTERNAL_ERROR
 #define RAISE_RUNTIME_ERROR_AT_LINE(X, Y) RAISE_RUNTIME_ERROR_AT(X, handle_error_at_line(Y))
-#define GET_ERROR_MSG(X, ...) snprintf(errors->msg, sizeof(char) * 1024, X, __VA_ARGS__)
+#ifdef __NDEBUG__
+#define ABORT_IF(X)
+#else
+#define ABORT_IF(X)           \
+    if (X) {                  \
+        RAISE_INTERNAL_ERROR; \
+    }
+#endif
 
 #endif
