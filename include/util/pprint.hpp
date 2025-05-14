7daadf0b2ba0fd216df9a0a384f1bbb7cd4b5bf5
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-14 12:31:01 +0300

    pass context to pprint

--- include/util/pprint.hpp
+++ include/util/pprint.hpp
@@ -17,15 +17,12 @@ struct IdentifierContext;
 // Pretty print
 
 void pprint_toks(const std::vector<Token>& tokens);
-void pprint_ast(Ast* node, const std::string& name);
-void pprint_addressed_set(void);
-void pprint_string_const_table(void);
-void pprint_struct_typedef_table(void);
-void pprint_symbol_table(void);
-void pprint_backend_symbol_table(void);
-void pprint_p_backend(BackEndContext* ctx);
-void pprint_p_frontend(FrontEndContext* ctx);
-void pprint_p_identifiers(IdentifierContext* ctx);
+void pprint_ast(IdentifierContext* ctx, Ast* node, const std::string& name);
+void pprint_addressed_set(IdentifierContext* ctx, FrontEndContext* frontend);
+void pprint_string_const_table(IdentifierContext* ctx, FrontEndContext* frontend);
+void pprint_struct_typedef_table(IdentifierContext* ctx, FrontEndContext* frontend);
+void pprint_symbol_table(IdentifierContext* ctx, FrontEndContext* frontend);
+void pprint_backend_symbol_table(IdentifierContext* ctx, BackEndContext* backend);
 
 #endif
 #endif
