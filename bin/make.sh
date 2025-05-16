e52749472721a09561e82464bfd5ee8634d0fa78
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-16 12:57:25 +0300

    make cmake build optional

--- bin/make.sh
+++ bin/make.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 cd ../build/
-./build.sh
-if [ ${?} -ne 0 ]; then
-    echo -e "\033[0;31merror:\033[0m build failed" 1>&2
-    exit 1
+if [ "${1}" = "--cmake" ]; then
+    ./cmake_build.sh
+    if [ ${?} -ne 0 ]; then
+        echo -e "\033[0;31merror:\033[0m build failed" 1>&2
+        exit 1
+    fi
+else
+    ./build.sh
+    if [ ${?} -ne 0 ]; then
+        echo -e "\033[0;31merror:\033[0m build failed" 1>&2
+        exit 1
+    fi
 fi
 
 echo -e "build was successful, install with \033[1m‘./install.sh’\033[0m"
