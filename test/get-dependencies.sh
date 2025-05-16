0f22eb7dde68ba40da4e511027c262fa8616b578
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-16 17:32:08 +0300

    improve get-dependencies script

--- test/get-dependencies.sh
+++ test/get-dependencies.sh
@@ -1,157 +1,118 @@
 #!/bin/bash
 
-function apt_install () {
-    i=0
-    sudo apt-get update
-    sudo apt-get -y upgrade
-    for PKG in $(echo "\
-        make      \
-        cmake     \
-        diffutils \
-        valgrind  \
-    "); do
-        if [ ${INSTALL_PKGS[${i}]} -eq 1 ]; then
-            sudo apt-get -y install ${PKG}
-        fi
-        i=$((i+1))
-    done
-}
-
-function dnf_install () {
-    i=0
-    sudo dnf -y upgrade
-    for PKG in $(echo "\
-        make.x86_64      \
-        cmake.x86_64     \
-        diffutils.x86_64 \
-        valgrind.x86_64  \
-    "); do
-        if [ ${INSTALL_PKGS[${i}]} -eq 1 ]; then
-            sudo dnf -y install ${PKG}
-        fi
-        i=$((i+1))
-    done
-}
-
-function pacman_install () {
-    i=0
-    sudo pacman -Syu
-    for PKG in $(echo "\
-        make      \
-        cmake     \
-        diffutils \
-        valgrind  \
-    "); do
-        if [ ${INSTALL_PKGS[${i}]} -eq 1 ]; then
-            sudo pacman -Syu ${PKG}
-        fi
-        i=$((i+1))
-    done
-}
-
-function no_install () {
-    if [ ! -z "${INSTALL_MSG}" ]; then
-        echo -e "\033[1;34mwarning:\033[0m install the following packages before testing: ${INSTALL_MSG}"
-    fi
-}
-
-function get_install () {
-    i=0
-    for PKG in $(echo "\
-        make      \
-        cmake     \
-        diffutils \
-        valgrind  \
-    "); do
-        if [ ${INSTALL_PKGS[${i}]} -eq 1 ]; then
-            if [ ! "${INSTALL_Y}" = "y" ]; then
-                INSTALL_Y="y"
-            else
-                INSTALL_MSG="${INSTALL_MSG}, "
-            fi
-            INSTALL_MSG="${INSTALL_MSG}\033[1m‘${PKG}’\033[0m"
-        fi
-        i=$((i+1))
-    done
-
-    if [ "${INSTALL_Y}" = "y" ]; then
-        echo -e -n "install missing dependencies ${INSTALL_MSG}? [y/n]: "
-        read -p "" INSTALL_Y
-    fi
-}
-
-INSTALL_Y=""
-INSTALL_MSG=""
-
-INSTALL_MAKE=0
-INSTALL_CMAKE=1
-INSTALL_DIFFUTILS=2
-INSTALL_VALGRIND=3
-
-INSTALL_PKGS=(
-    0 0 0 0
-)
+INSTALL_PKGS=(0 0 0 0)
+INSTALL_DEPS="make cmake diffutils valgrind"
 
 make --help > /dev/null 2>&1
 if [ ${?} -ne 0 ]; then
-    INSTALL_PKGS[${INSTALL_MAKE}]=1
+    INSTALL_PKGS[0]=1
 fi
 
 cmake --help > /dev/null 2>&1
 if [ ${?} -ne 0 ]; then
-    INSTALL_PKGS[${INSTALL_CMAKE}]=1
+    INSTALL_PKGS[1]=1
 fi
 
 diff --help > /dev/null 2>&1
 if [ ${?} -ne 0 ]; then
-    INSTALL_PKGS[${INSTALL_DIFFUTILS}]=1
+    INSTALL_PKGS[2]=1
 fi
 
 valgrind --help > /dev/null 2>&1
 if [ ${?} -ne 0 ]; then
-    INSTALL_PKGS[${INSTALL_VALGRIND}]=1
+    INSTALL_PKGS[3]=1
 fi
 
-get_install
+INSTALL_Y="n"
+for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+    if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+        DEP="$(echo "${INSTALL_DEPS}" | cut -d" " -f${i})"
+        if [ "${INSTALL_Y}" = "y" ]; then
+            echo -e -n ", \033[1m‘${DEP}’\033[0m"
+        else
+            echo -e -n "install missing dependencies \033[1m‘${DEP}’\033[0m"
+            INSTALL_Y="y"
+        fi
+    fi
+done
+if [ "${INSTALL_Y}" = "y" ]; then
+    echo -e -n "? [y/n]: "
+    read -p "" INSTALL_Y
+fi
 
 if [ "${INSTALL_Y}" = "y" ]; then
     DISTRO="$(cat /etc/os-release | grep -P "^NAME=" | cut -d"\"" -f2)"
     case "${DISTRO}" in
-        # Debian-based
         "Debian GNU/Linux") ;&
-        "elementary OS") ;&
-        "Kali GNU/Linux") ;&
         "Linux Mint") ;&
-        "Pop!_OS") ;&
         "Ubuntu")
-            apt_install
+            sudo apt-get update && {
+                for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+                    if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+                        DEP="$(echo "${INSTALL_DEPS}" | cut -d" " -f${i})"
+                        sudo apt-get -y install ${DEP}
+                        INSTALL_PKGS[$((i-1))]=${?}
+                    fi
+                done
+            }
             ;;
-        # RPM-based
-        "AlmaLinux") ;&
-        "CentOS Linux") ;&
-        "CentOS Stream") ;&
-        "Clear Linux OS") ;&
-        "ClearOS") ;&
-        "Fedora") ;&
-        "Fedora Linux") ;&
-        "Mageia") ;&
         "openSUSE Leap") ;&
-        "Red Hat Enterprise Linux") ;&
         "Rocky Linux")
-            dnf_install
+            sudo dnf check-update && {
+                for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+                    if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+                        DEP="$(echo "${INSTALL_DEPS}" | cut -d" " -f${i})"
+                        sudo dnf -y install ${DEP}.x86_64
+                        INSTALL_PKGS[$((i-1))]=${?}
+                    fi
+                done
+            }
             ;;
-        # Pacman-based
         "Arch Linux") ;&
-        "EndeavourOS") ;&
-        "Manjaro")
-            pacman_install
+        "EndeavourOS")
+            sudo pacman -Syy && {
+                for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+                    if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+                        DEP="$(echo "${INSTALL_DEPS}" | cut -d" " -f${i})"
+                        sudo pacman -Syu ${DEP}
+                        INSTALL_PKGS[$((i-1))]=${?}
+                    fi
+                done
+            }
             ;;
-        # Other
+        # Not tested yet
+        # "elementary OS") ;&
+        # "Kali GNU/Linux") ;&
+        # "Pop!_OS") ;&
+        # "AlmaLinux") ;&
+        # "CentOS Linux") ;&
+        # "CentOS Stream") ;&
+        # "Clear Linux OS") ;&
+        # "ClearOS") ;&
+        # "Fedora") ;&
+        # "Fedora Linux") ;&
+        # "Mageia") ;&
+        # "Red Hat Enterprise Linux") ;&
+        # "Manjaro") ;&
         *)
-            no_install
+            echo -e "\033[1;34mwarning:\033[0m this distribution was not tested yet, use at your own risk!"
+            INSTALL_Y="n"
     esac
-else
-    no_install
 fi
+
+if [ "${INSTALL_Y}" = "y" ]; then
+    for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+        if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+            DEP="$(echo "${INSTALL_DEPS}" | cut -d" " -f${i})"
+            echo -e "\033[1;34mwarning:\033[0m failed to install \033[1m‘${DEP}’\033[0m"
+        fi
+    done
+fi
+for i in $(seq 1 ${#INSTALL_PKGS[@]}); do
+    if [ ${INSTALL_PKGS[$((i-1))]} -ne 0 ]; then
+        echo -e "\033[1;34mwarning:\033[0m install the missing dependencies before testing"
+        break
+    fi
+done
 
 exit 0
