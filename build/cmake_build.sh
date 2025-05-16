41531fa0a5bff3c2571ed53f93e8920b4d9f0ac8
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-16 12:11:57 +0300

    add build_preset.sh

--- build/cmake_build.sh
+++ build/cmake_build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-cd ../build/
-
 ./build_preset.sh
 if [ ${?} -ne 0 ]; then exit 1; fi
 
 export CC=$(which gcc)
 export CXX=$(which g++)
 
-cmake -G "Unix Makefiles" -S . -B test/ -DCMAKE_BUILD_TYPE=Release
+cmake -G "Unix Makefiles" -S . -B release/ -DCMAKE_BUILD_TYPE=Release -D__NDEBUG__=ON
 if [ ${?} -ne 0 ]; then exit 1; fi
 
-cd test/
+cd release/
 cmake --build . --config Release
 if [ ${?} -ne 0 ]; then exit 1; fi
 
