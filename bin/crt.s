45dc636c63948a070ebe024614715db260ece75a
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-19 17:27:30 +0300

    use libc exit in crt

--- bin/crt.s
+++ bin/crt.s
@@ -2,23 +2,12 @@
     .text
 _start:
     xorl %ebp, %ebp
-    xorl %eax, %eax
     movl (%rsp), %edi
     leaq 8(%rsp), %rsi
     leaq 16(%rsp, %rdi, 8), %rdx
-    pushq %rbp
-    movq %rsp, %rbp
-        subq $8, %rsp
-        pushq %rbx
-        call main
-        movl %eax, %ebx
-        xorl %eax, %eax
-        xorl %edi, %edi
-        call fflush
-        movl %ebx, %edi
-        popq %rbx
-    movq %rbp, %rsp
-    popq %rbp
     xorl %eax, %eax
-    call _exit
+    call main
+    movl %eax, %edi
+    xorl %eax, %eax
+    call exit
         .section .note.GNU-stack,"",@progbits
