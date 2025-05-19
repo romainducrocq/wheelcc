0382b956bee6249017f35278a80bb941c3cd0306
romainducrocq <romain.g.ducrocq@gmail.com>
2025-05-19 16:44:54 +0300

    flush before exit crt

--- bin/crt.s
+++ bin/crt.s
@@ -2,12 +2,23 @@
     .text
 _start:
     xor %ebp, %ebp
-    mov (%rsp), %edi
-    lea 8(%rsp), %rsi
-    lea 16(%rsp, %rdi, 8), %rdx
     xor %eax, %eax
-    call main
-    mov %eax, %edi
+    movl (%rsp), %edi
+    leaq 8(%rsp), %rsi
+    leaq 16(%rsp, %rdi, 8), %rdx
+    pushq %rbp
+    movq %rsp, %rbp
+        subq $8, %rsp
+        pushq %r15
+        call main
+        movl %eax, %r15d
+        xor %eax, %eax
+        xor %edi, %edi
+        call fflush
+        movl %r15d, %edi
+        popq %r15
+    movq %rbp, %rsp
+    popq %rbp
     xor %eax, %eax
     call _exit
         .section .note.GNU-stack,"",@progbits
