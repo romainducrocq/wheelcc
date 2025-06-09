9f0e927332697159f1adc533d1e0a8875baa73b2
romainducrocq <romain.g.ducrocq@gmail.com>
2025-06-09 15:37:43 +0300

    prepare lexer case macros

--- src/frontend/parser/lexer.cpp
+++ src/frontend/parser/lexer.cpp
@@ -34,6 +34,77 @@ typedef LexerContext* Ctx;
 
 static void tokenize_include(Ctx ctx, std::string include_match, size_t linenum);
 
+#define CASE_W \
+    CASE_DIGIT \
+    CASE_LETTER
+
+#define CASE_DIGIT \
+    case "0":      \
+    case "1":      \
+    case "2":      \
+    case "3":      \
+    case "4":      \
+    case "5":      \
+    case "6":      \
+    case "7":      \
+    case "8":      \
+    case "9":
+
+#define CASE_LETTER \
+    case "_":       \
+    case "a":       \
+    case "b":       \
+    case "c":       \
+    case "d":       \
+    case "e":       \
+    case "f":       \
+    case "g":       \
+    case "h":       \
+    case "i":       \
+    case "j":       \
+    case "k":       \
+    case "l":       \
+    case "m":       \
+    case "n":       \
+    case "o":       \
+    case "p":       \
+    case "q":       \
+    case "r":       \
+    case "s":       \
+    case "t":       \
+    case "u":       \
+    case "v":       \
+    case "w":       \
+    case "x":       \
+    case "y":       \
+    case "z":       \
+    case "A":       \
+    case "B":       \
+    case "C":       \
+    case "D":       \
+    case "E":       \
+    case "F":       \
+    case "G":       \
+    case "H":       \
+    case "I":       \
+    case "J":       \
+    case "K":       \
+    case "L":       \
+    case "M":       \
+    case "N":       \
+    case "O":       \
+    case "P":       \
+    case "Q":       \
+    case "R":       \
+    case "S":       \
+    case "T":       \
+    case "U":       \
+    case "V":       \
+    case "W":       \
+    case "X":       \
+    case "Y":       \
+    case "Z":
+
 #define RE_MATCH_TOKEN(X, Y)                                                                 \
     {                                                                                        \
         CTLL_FIXED_STRING re_pattern = {X};                                                  \
