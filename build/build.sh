082b388710e7923f015e8a4f0fa47478c12fe304
romainducrocq <romain.g.ducrocq@gmail.com>
2025-07-04 19:03:04 +0300

    build objects with cache

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 PACKAGE_NAME="$(cat ../bin/package_name.txt)"
+CC="gcc"
 CXX="g++"
 if [[ "$(uname -s)" = "Darwin"* ]]; then
+    CC="clang"
     CXX="clang++"
 fi
 
@@ -12,16 +14,18 @@ if [ ${?} -ne 0 ]; then exit 1; fi
 BUILD_RELEASE=0
 PROJECT_DIR="$(dirname $(dirname $(readlink -f ${0})))"
 
-CXX_FLAGS="-std=c++17 -Wall -Wextra -Wpedantic"
-CXX_FLAGS_RELEASE="-O3 -DNDEBUG -Werror -pedantic-errors -D__NDEBUG__"
-CXX_FLAGS_DEBUG="-ggdb3 -O0"
+CC="${CC} -std=c17"
+CXX="${CXX} -std=c++17"
+CC_FLAGS="-Wall -Wextra -Wpedantic"
+CC_FLAGS_RELEASE="-O3 -DNDEBUG -Werror -pedantic-errors -D__NDEBUG__"
+CC_FLAGS_DEBUG="-ggdb3 -O0"
 if [ ${BUILD_RELEASE} -eq 0 ]; then
-    CXX_FLAGS="${CXX_FLAGS} ${CXX_FLAGS_RELEASE}"
+    CC_FLAGS="${CC_FLAGS} ${CC_FLAGS_RELEASE}"
 else
-    CXX_FLAGS="${CXX_FLAGS} ${CXX_FLAGS_DEBUG}"
+    CC_FLAGS="${CC_FLAGS} ${CC_FLAGS_DEBUG}"
 fi
 
-PROJECT_NAME="-o ${PROJECT_DIR}/bin/${PACKAGE_NAME}"
+PROJECT_NAME="${PROJECT_DIR}/bin/${PACKAGE_NAME}"
 
 INCLUDE_DIRS="-I${PROJECT_DIR}/include/"
 INCLUDE_DIRS="${INCLUDE_DIRS} -I${PROJECT_DIR}/src/ast/"
@@ -57,7 +61,39 @@ SOURCE_FILES="${SOURCE_FILES} ${PROJECT_DIR}/src/util/throw.cpp"
 SOURCE_FILES="${SOURCE_FILES} ${PROJECT_DIR}/lib/sds/sds.c"
 SOURCE_FILES="${SOURCE_FILES} ${PROJECT_DIR}/lib/stb_ds/stb_ds.c"
 
-${CXX} ${SOURCE_FILES} ${CXX_FLAGS} ${INCLUDE_DIRS} ${PROJECT_NAME}
+BUILD_CACHE="${PROJECT_DIR}/build/build_cache"
+if [ -d "${BUILD_CACHE}/" ]; then
+    rm -r ${BUILD_CACHE}/
+    if [ ${?} -ne 0 ]; then exit 1; fi
+fi
+mkdir ${BUILD_CACHE}/
 if [ ${?} -ne 0 ]; then exit 1; fi
+
+OBJECT_FILES=""
+echo "build objects ..."
+for FILE in ${SOURCE_FILES}; do
+    OBJECT="${BUILD_CACHE}/$(basename ${FILE%.*}).o"
+    OBJECT_FILES="${OBJECT_FILES} ${OBJECT}"
+    echo "${FILE} -> ${OBJECT}"
+    case "${FILE##*.}" in
+        "c")
+            ${CC} -c ${FILE} ${CC_FLAGS} ${INCLUDE_DIRS} -o ${OBJECT}
+            if [ ${?} -ne 0 ]; then exit 1; fi
+            ;;
+        "cpp")
+            ${CXX} -c ${FILE} ${CC_FLAGS} ${INCLUDE_DIRS} -o ${OBJECT}
+            if [ ${?} -ne 0 ]; then exit 1; fi
+            ;;
+        *)
+            exit 1
+    esac
+done
+
+echo "linking executable ..."
+echo "${BUILD_CACHE}/*.o -> ${PROJECT_NAME}"
+${CXX} ${OBJECT_FILES} ${CC_FLAGS} -o ${PROJECT_NAME}
+if [ ${?} -ne 0 ]; then exit 1; fi
+
+echo "created target ${PROJECT_NAME}"
 
 exit 0
