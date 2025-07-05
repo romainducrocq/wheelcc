5d738eb66b2c75267340eee453a548fd065d0790
romainducrocq <romain.g.ducrocq@gmail.com>
2025-07-05 17:16:24 +0200

    add str api

--- include/util/c_std.hpp
+++ include/util/c_std.hpp
@@ -8,6 +8,49 @@
 
 // C std
 
-// TODO
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// String
+
+#define string_t sds
+#define str_delete(X) \
+    if (X) {          \
+        sdsfree(X);   \
+        X = NULL;     \
+    }
+#define str_new(X) X ? sdsnew(X) : NULL
+#define str_move(X, Y) \
+    if (!*Y) {         \
+        *Y = *X;       \
+        *X = NULL;     \
+    }
+#define str_size(X) sdslen(X)
+#define str_append(X, Y)  \
+    do {                  \
+        X = sdscat(X, Y); \
+    }                     \
+    while (0)
+#define str_copy(X, Y) \
+    do {               \
+        str_delete(Y); \
+        Y = sdsdup(X); \
+    }                  \
+    while (0)
+#define str_to_string(X) sdsfromlonglong((long long)(X))
+// #define str_substr_view(X, Y, Z) sdsrange(X, Y, Z)
+#define str_substr(X, Y, Z, S) \
+    do {                       \
+        str_copy(X, S);        \
+        sdsrange(S, Y, Z);     \
+    }                          \
+    while (0)
+// #define str_reserve
+#define str_resize(X, Y)       \
+    do {                       \
+        X = sdsgrowzero(X, Y); \
+    }                          \
+    while (0)
+#define str_clear(X) sdsclear(X)
+#define str_back(X) (X)[str_size(X) - 1]
 
 #endif
