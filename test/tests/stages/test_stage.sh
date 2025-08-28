4fd68b2e89c4a8ce643e248ec8bbacfe383de7f2
romainducrocq <romain.g.ducrocq@gmail.com>
2025-08-28 14:13:17 +0300

    update test_stage

--- test/tests/stages/test_stage.sh
+++ test/tests/stages/test_stage.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 STAGE="${1}"
+if [[ "${1}" = "codegen_no"* ]]; then
+    STAGE="codegen"
+fi
+
 cd ../compiler
 for FILE in $(find . -name "*.c" -type f); do
     STDOUT=$(wheelcc --${STAGE} -O0 ${FILE} 2> /dev/null)
-    diff ../stages/${FILE%.*}.${STAGE} <(echo "${STDOUT}")
+    diff ../stages/${FILE%.*}.${1} <(echo "${STDOUT}")
 done
