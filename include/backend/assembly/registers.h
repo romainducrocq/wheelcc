8cfdc1d3cdc4f71782897f784fc9283caa11e83a
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-08 17:32:51 +0300

    registers with c ast

--- include/backend/assembly/registers.h
+++ include/backend/assembly/registers.h
@@ -8,9 +8,7 @@
 #include "assembly/regs.h" // backend
 
 typedef struct AsmReg AsmReg;
-typedef struct AsmRegister AsmRegister;
-typedef struct AsmMemory AsmMemory;
-typedef struct AsmIndexed AsmIndexed;
+typedef struct AsmOperand AsmOperand;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -19,9 +17,9 @@ typedef struct AsmIndexed AsmIndexed;
 #ifdef __cplusplus
 extern "C" {
 #endif
-shared_ptr_t(AsmRegister) gen_register(REGISTER_KIND reg_kind);
-shared_ptr_t(AsmMemory) gen_memory(REGISTER_KIND reg_kind, TLong value);
-shared_ptr_t(AsmIndexed) gen_indexed(REGISTER_KIND reg_kind_base, REGISTER_KIND reg_kind_idx, TLong scale);
+shared_ptr_t(AsmOperand) gen_register(REGISTER_KIND reg_kind);
+shared_ptr_t(AsmOperand) gen_memory(REGISTER_KIND reg_kind, TLong value);
+shared_ptr_t(AsmOperand) gen_indexed(REGISTER_KIND reg_kind_base, REGISTER_KIND reg_kind_idx, TLong scale);
 REGISTER_KIND register_mask_kind(AsmReg* node);
 size_t register_mask_bit(REGISTER_KIND reg_kind);
 bool register_mask_get(TULong reg_mask, REGISTER_KIND reg_kind);
