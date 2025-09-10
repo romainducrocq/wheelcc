6e1653d093086988f2dfe87eb7e108d62684831b
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-10 14:33:09 +0300

    gas_code interface with c ast

--- include/backend/emitter/gas_code.h
+++ include/backend/emitter/gas_code.h
@@ -1,7 +1,7 @@
 #ifndef _BACK_EMITTER_GAS_CODE_H
 #define _BACK_EMITTER_GAS_CODE_H
 
-#include <memory>
+#include "util/c_std.h"
 
 typedef struct AsmProgram AsmProgram;
 typedef struct BackEndContext BackEndContext;
@@ -12,13 +12,13 @@ typedef struct IdentifierContext IdentifierContext;
 
 // Gnu assembler code emission
 
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-void emit_gas_code(std::unique_ptr<AsmProgram>* asm_ast, BackEndContext* backend, FileIoContext* fileio,
-    IdentifierContext* identifiers);
-// #ifdef __cplusplus
-// }
-// #endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+void emit_gas_code(
+    unique_ptr_t(AsmProgram) * asm_ast, BackEndContext* backend, FileIoContext* fileio, IdentifierContext* identifiers);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
