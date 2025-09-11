a16d1b2185316815457d7f7f61d12cbcb9eb8baf
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-11 16:18:20 +0300

    clean tools

--- test/tools/replace_all.sh
+++ test/tools/replace_all.sh
@@ -3,7 +3,7 @@
 FILE="$(readlink -f ${1})"
 
 while read LINE; do
-    EXPR=$(echo "${LINE}" | cut -d" " -f1)
-    REPLACE=$(echo "${LINE}" | cut -d" " -f2)
+    EXPR=$(echo "${LINE}" | cut -d"@" -f1)
+    REPLACE=$(echo "${LINE}" | cut -d"@" -f2)
     sed -i "s/\<${EXPR}\>/${REPLACE}/g" ${FILE}
 done < <(cat "${2}")
